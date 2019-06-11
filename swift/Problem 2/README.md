b5a51d276a75a12a3620286ff2f3f442ff3269bd
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-10 20:45:32 -0500

    Added problem 5

--- swift/Problem 2/README.md
+++ swift/Problem 2/README.md
@@ -1,7 +1,5 @@
 ### Description
 
-Good morning! Here's your coding interview problem for today.
-
 This problem was asked by Uber.
 
 Given an array of integers, return a new array such that each element at index `i` of the new array is the product of all the numbers in the original array except the one at `i`.
---
+++ swift/Problem 5/Problem 5/Problem 5/Problem_5.h
@@ -0,0 +1,19 @@
+//
+//  Problem_5.h
+//  Problem 5
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-10.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_5.
+FOUNDATION_EXPORT double Problem_5VersionNumber;
+
+//! Project version string for Problem_5.
+FOUNDATION_EXPORT const unsigned char Problem_5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_5/PublicHeader.h>
+
+
