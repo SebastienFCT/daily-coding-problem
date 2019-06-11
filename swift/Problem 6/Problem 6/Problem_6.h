84b735f6b71c63075d1f121d56d427d5b2c55e37
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-10 21:53:31 -0500

    Added problem 6

---
+++ swift/Problem 6/Problem 6/Problem_6.h
@@ -0,0 +1,19 @@
+//
+//  Problem_6.h
+//  Problem 6
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-10.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_6.
+FOUNDATION_EXPORT double Problem_6VersionNumber;
+
+//! Project version string for Problem_6.
+FOUNDATION_EXPORT const unsigned char Problem_6VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_6/PublicHeader.h>
+
+
