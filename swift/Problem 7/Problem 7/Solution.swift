72708fb0157c558dd788928391ea4b8f3d73e2d3
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-11 23:37:02 -0500

    pushed solution for problem 7

--- swift/Problem 7/Problem 7/Solution.swift
+++ swift/Problem 7/Problem 7/Solution.swift
@@ -1,16 +1,13 @@
-### Description
+//
+//  Solution.swift
+//  Problem 7
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-11.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Facebook.
+import Foundation
 
-Given the mapping `a = 1, b = 2, ... z = 26`, and an encoded message, count the number of ways it can be decoded.
-
-For example, the message `111` would give `3`, since it could be decoded as `aaa`, `ka`, and `ak`.
-
-You can assume that the messages are decodable. For example, `001` is not allowed.
-
-### Solution
-
-```swift
 extension String {
     func decypher() -> Int {
         let substring = String(self.prefix(2))
@@ -36,4 +33,3 @@ extension String {
         return 0
     }
 }
-```
