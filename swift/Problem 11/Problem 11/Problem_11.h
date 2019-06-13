50dca9275e529aaa4b1bfaefa851c9afeec9b419
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-12 21:55:21 -0500

    adding problem 11

---
+++ swift/Problem 11/Problem 11/Problem_11.h
@@ -0,0 +1,19 @@
+//
+//  Problem_11.h
+//  Problem 11
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-12.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_11.
+FOUNDATION_EXPORT double Problem_11VersionNumber;
+
+//! Project version string for Problem_11.
+FOUNDATION_EXPORT const unsigned char Problem_11VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_11/PublicHeader.h>
+
+
