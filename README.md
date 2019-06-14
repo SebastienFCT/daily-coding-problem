1ac93948a05e6151c3982cd34fe9c3782eba2ac3
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-13 21:46:51 -0500

    added problem 14

--- README.md
+++ README.md
@@ -252,3 +252,18 @@ For example, given `s = "abcba"` and `k = 2`, the longest substring with `k` dis
 
 
 * [Swift](./swift/Problem&#32;13/)
+
+## Problem 14
+
+#### Description
+
+This problem was asked by Google.
+
+The area of a circle is defined as `πr^2`. Estimate `π` to `3` decimal places using a Monte Carlo method.
+
+Hint: The basic equation of a circle is `x^2 + y^2 = r^2`.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;14/)
---
+++ swift/Problem 14/Problem 14/Problem_14.h
@@ -0,0 +1,19 @@
+//
+//  Problem_14.h
+//  Problem 14
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-13.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_14.
+FOUNDATION_EXPORT double Problem_14VersionNumber;
+
+//! Project version string for Problem_14.
+FOUNDATION_EXPORT const unsigned char Problem_14VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_14/PublicHeader.h>
+
+
