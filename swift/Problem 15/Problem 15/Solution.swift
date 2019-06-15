fa377cad09f82d99df3684724df337678c0a8787
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-14 21:09:31 -0500

    Solution to problem 15

--- swift/Problem 15/Problem 15/Solution.swift
+++ swift/Problem 15/Problem 15/Solution.swift
@@ -1,22 +1,13 @@
-### Description
+//
+//  Solution.swift
+//  Problem 15
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-14.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Facebook.
+import Foundation
 
-Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
-
-### Solution
-
-```swift
-func testExample() {
-    var random = Random()
-    let max = 30
-    
-    for i in 0..<max {
-        print("After reading \(i) value(s). Random = \(random.selectRandom(x: i))")
-    }
-}
-
-    
 struct Random {
     
     var result = 0
@@ -38,4 +29,3 @@ struct Random {
         return result
     }
 }
-```
