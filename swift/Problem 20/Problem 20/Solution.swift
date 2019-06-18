7d078918b5e38fd755a00a2b20220a257eb64251
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-17 20:05:34 -0500

    Solution for problem 20

--- swift/Problem 20/Problem 20/Solution.swift
+++ swift/Problem 20/Problem 20/Solution.swift
@@ -1,18 +1,13 @@
-### Description
+//
+//  Solution.swift
+//  Problem 20
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-17.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
-
-For example, given `A = 3 -> 7 -> 8 -> 10` and `B = 99 -> 1 -> 8 -> 10`, return the node with value `8`.
-
-In this example, assume nodes with the same value are the exact same node objects.
-
-Do this in `O(M + N)` time (where `M` and `N` are the lengths of the lists) and constant space.
-
-### Solution
-
-```swift
 class Node: Equatable, Comparable, Hashable {
     var value: Int
     var next: Node?
@@ -61,19 +56,3 @@ func commonNode(lhs: Node, rhs: Node) -> Node? {
     
     return nil
 }
-```
-
-#### Test
-
-```swift
-class Problem_20Tests: XCTestCase {
-
-    func test() {
-        let input1 = Node(value: 3, next: Node(value: 7, next: Node(value: 8, next: Node(value: 10, next: nil))))
-        let input2 = Node(value: 99, next: Node(value: 1, next: Node(value: 8, next: Node(value: 10, next: nil))))
-        
-        XCTAssert(commonNode(lhs: input1, rhs: input2)!.value == 8)
-    }
-
-}
-```
--- swift/Problem 20/Problem 20Tests/Problem_20Tests.swift
+++ swift/Problem 20/Problem 20Tests/Problem_20Tests.swift
@@ -11,24 +11,11 @@ import XCTest
 
 class Problem_20Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test() {
+        let input1 = Node(value: 3, next: Node(value: 7, next: Node(value: 8, next: Node(value: 10, next: nil))))
+        let input2 = Node(value: 99, next: Node(value: 1, next: Node(value: 8, next: Node(value: 10, next: nil))))
+        
+        XCTAssert(commonNode(lhs: input1, rhs: input2)!.value == 8)
     }
 
 }
