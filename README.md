b241f420394c91d96c74ff5ff762612277b7fde1
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-18 20:53:54 -0500

    added problem 21

--- README.md
+++ README.md
@@ -404,3 +404,18 @@ Do this in `O(M + N)` time (where `M` and `N` are the lengths of the lists) and 
 
 
 * [Swift](./swift/Problem&#32;20/)
+
+## Problem 21
+
+#### Description
+
+This problem was asked by Snapchat.
+
+Given an array of time intervals `(start, end)` for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+
+For example, given `[(30, 75), (0, 50), (60, 150)]`, you should return 2.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;21/)
---
+++ swift/Problem 21/Problem 21/Problem_21.h
@@ -0,0 +1,19 @@
+//
+//  Problem_21.h
+//  Problem 21
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-18.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_21.
+FOUNDATION_EXPORT double Problem_21VersionNumber;
+
+//! Project version string for Problem_21.
+FOUNDATION_EXPORT const unsigned char Problem_21VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_21/PublicHeader.h>
+
+
