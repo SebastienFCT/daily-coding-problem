fcd5465adf98df323e603b3020fa8e9b09f7c430
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-06-24 18:51:27 -0500

    Adding Problem 30

---
+++ swift/Problem 30/Problem 30/Problem_30.h
@@ -0,0 +1,19 @@
+//
+//  Problem_30.h
+//  Problem 30
+//
+//  Created by sebastien FOCK CHOW THO on 2019-06-24.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_30.
+FOUNDATION_EXPORT double Problem_30VersionNumber;
+
+//! Project version string for Problem_30.
+FOUNDATION_EXPORT const unsigned char Problem_30VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_30/PublicHeader.h>
+
+
