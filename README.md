83b286a6b6608351de0eb73ec6c022eac2da5a48
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-01 11:06:54 -0500

    Added problem 37

--- README.md
+++ README.md
@@ -708,3 +708,20 @@ Given the root to a binary search tree, find the second largest node in the tree
 
 
 * [Swift](./swift/Problem&#32;36/)
+
+## Problem 37
+
+#### Description
+
+This problem was asked by Google.
+
+The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set `{1, 2, 3}`, it should return `{{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}`.
+
+You may also use a list or array to represent a set.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;37/)
---
+++ swift/Problem 37/Problem 37/Problem_37.h
@@ -0,0 +1,19 @@
+//
+//  Problem_37.h
+//  Problem 37
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-01.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_37.
+FOUNDATION_EXPORT double Problem_37VersionNumber;
+
+//! Project version string for Problem_37.
+FOUNDATION_EXPORT const unsigned char Problem_37VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_37/PublicHeader.h>
+
+
