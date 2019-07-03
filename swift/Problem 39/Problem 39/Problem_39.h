9563a0ba30b4e02b9ce29ab1c3ced82783a4e297
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-03 15:02:48 -0500

    Added problem 39

---
+++ swift/Problem 39/Problem 39/Problem_39.h
@@ -0,0 +1,19 @@
+//
+//  Problem_39.h
+//  Problem 39
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-03.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_39.
+FOUNDATION_EXPORT double Problem_39VersionNumber;
+
+//! Project version string for Problem_39.
+FOUNDATION_EXPORT const unsigned char Problem_39VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_39/PublicHeader.h>
+
+
