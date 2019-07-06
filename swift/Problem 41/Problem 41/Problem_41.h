76093d64e346cf57b5147edc8087d05e256326bc
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-05 23:15:25 -0500

    Added problem 41

---
+++ swift/Problem 41/Problem 41/Problem_41.h
@@ -0,0 +1,19 @@
+//
+//  Problem_41.h
+//  Problem 41
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-05.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_41.
+FOUNDATION_EXPORT double Problem_41VersionNumber;
+
+//! Project version string for Problem_41.
+FOUNDATION_EXPORT const unsigned char Problem_41VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_41/PublicHeader.h>
+
+
