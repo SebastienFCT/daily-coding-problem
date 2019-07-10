c98f5e73adfd674e315c28da9fec873c2d438025
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-10 17:08:40 -0500

    Adding problem 46

---
+++ swift/Problem 46/Problem 46/Problem_46.h
@@ -0,0 +1,19 @@
+//
+//  Problem_46.h
+//  Problem 46
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-10.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_46.
+FOUNDATION_EXPORT double Problem_46VersionNumber;
+
+//! Project version string for Problem_46.
+FOUNDATION_EXPORT const unsigned char Problem_46VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_46/PublicHeader.h>
+
+
