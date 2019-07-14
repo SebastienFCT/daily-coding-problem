9e9f86a4df8fcbd431fc0f78cff4c85b2915527d
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-14 14:13:21 -0500

    Adding problem 50

---
+++ swift/Problem 50/Problem 50/Problem_50.h
@@ -0,0 +1,19 @@
+//
+//  Problem_50.h
+//  Problem 50
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-14.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_50.
+FOUNDATION_EXPORT double Problem_50VersionNumber;
+
+//! Project version string for Problem_50.
+FOUNDATION_EXPORT const unsigned char Problem_50VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_50/PublicHeader.h>
+
+
---
+++ swift/Problem 50/README.md
@@ -0,0 +1,30 @@
+## Description
+
+This problem was asked by Microsoft.
+
+Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+
+Given the root to such a tree, write a function to evaluate it.
+
+For example, given the following tree:
+
+```
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+```
+
+You should return 45, as it is (3 + 2) * (4 + 5).
+
+## Solution
+
+```swift
+// MARK: - TODO
+```
+## Test
+
+```swift
+// MARK: - TODO
+```
