7596bdc206ef60a2785f724b8431ee799f0dfad5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-07-30 22:53:48 -0500

    Added problem 66

--- README.md
+++ README.md
@@ -1258,3 +1258,19 @@ You should print out the following:
 
 
 * [Swift](./swift/Problem&#32;65/)
+
+## Problem 66
+
+#### Description
+
+
+This problem was asked by Square.
+
+Assume you have access to a function toss_biased() which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the bias of the coin.
+
+Write a function to simulate an unbiased coin toss.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;66/)
---
+++ swift/Problem 66/Problem 66/Problem_66.h
@@ -0,0 +1,19 @@
+//
+//  Problem_66.h
+//  Problem 66
+//
+//  Created by sebastien FOCK CHOW THO on 2019-07-30.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_66.
+FOUNDATION_EXPORT double Problem_66VersionNumber;
+
+//! Project version string for Problem_66.
+FOUNDATION_EXPORT const unsigned char Problem_66VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_66/PublicHeader.h>
+
+
