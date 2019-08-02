a5ae663ebf2eceea6829157bd533a52bd28ebb8a
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-02 18:25:46 -0500

    commit to take finish later

---
+++ swift/Problem 69/Problem 69/Solution.swift
@@ -0,0 +1,17 @@
+//
+//  Solution.swift
+//  Problem 69
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-02.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+import Foundation
+
+/**
+ 
+    The example shows clearly the challenge: negative values can be used when their product result in a positive value
+ 
+    To solve this problem, I would order the array descendently and I would try to figure out an algorithm that find the 3 largest values with either 2 of them being negative or none of them
+ 
+ */
