84c94e89040272a4dcf518ef4fd54b6963c6caf3
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-08 18:39:18 -0500

    Added problem 75

--- README.md
+++ README.md
@@ -1464,3 +1464,18 @@ And there are 4 12's in the table.
 
 
 * [Swift](./swift/Problem&#32;74/)
+
+## Problem 75
+
+#### Description
+
+This problem was asked by Microsoft.
+
+Given an array of numbers, find the length of the longest increasing subsequence in the array. The subsequence does not necessarily have to be contiguous.
+
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;75/)
---
+++ swift/Problem 75/Problem 75/Problem_75.h
@@ -0,0 +1,19 @@
+//
+//  Problem_75.h
+//  Problem 75
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-08.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_75.
+FOUNDATION_EXPORT double Problem_75VersionNumber;
+
+//! Project version string for Problem_75.
+FOUNDATION_EXPORT const unsigned char Problem_75VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_75/PublicHeader.h>
+
+
