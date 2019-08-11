67f9fd7a44aba8a08f58ee2c447f4372fda431e2
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-11 07:28:05 -0500

    Added problem 78

---
+++ swift/Problem 78/Problem 78/Problem_78.h
@@ -0,0 +1,19 @@
+//
+//  Problem_78.h
+//  Problem 78
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-11.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_78.
+FOUNDATION_EXPORT double Problem_78VersionNumber;
+
+//! Project version string for Problem_78.
+FOUNDATION_EXPORT const unsigned char Problem_78VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_78/PublicHeader.h>
+
+
---
+++ swift/Problem 78/README.md
@@ -0,0 +1,17 @@
+## Description
+
+This problem was asked by Google.
+
+Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+
+## Solution
+
+```swift
+// MARK: - TODO
+```
+
+## Test
+
+```swift
+// MARK: - TODO
+```
