45c364c0d841d45a41ec5f0c576f114a0a889783
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-12 17:08:38 -0500

    Adding problem 79

---
+++ swift/Problem 79/Problem 79/Problem_79.h
@@ -0,0 +1,19 @@
+//
+//  Problem_79.h
+//  Problem 79
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-12.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_79.
+FOUNDATION_EXPORT double Problem_79VersionNumber;
+
+//! Project version string for Problem_79.
+FOUNDATION_EXPORT const unsigned char Problem_79VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_79/PublicHeader.h>
+
+
