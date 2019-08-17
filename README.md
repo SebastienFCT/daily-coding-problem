61f5c5829afc01324997e6e3458ca7210edd09f9
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-17 08:28:12 -0500

    Added problem 84

--- README.md
+++ README.md
@@ -1659,3 +1659,27 @@ should become:
 
 
 * [Swift](./swift/Problem&#32;83/)
+
+## Problem 84
+
+#### Description
+
+This problem was asked by Amazon.
+
+Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
+
+For example, this matrix has 4 islands.
+
+```
+1 0 0 0 0
+0 0 1 1 0
+0 1 1 0 0
+0 0 0 0 0
+1 1 0 0 1
+1 1 0 0 1
+```
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;84/)
---
+++ swift/Problem 84/Problem 84/Problem_84.h
@@ -0,0 +1,19 @@
+//
+//  Problem_84.h
+//  Problem 84
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-17.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_84.
+FOUNDATION_EXPORT double Problem_84VersionNumber;
+
+//! Project version string for Problem_84.
+FOUNDATION_EXPORT const unsigned char Problem_84VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_84/PublicHeader.h>
+
+
