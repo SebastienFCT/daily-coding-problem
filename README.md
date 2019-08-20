f5e32b725487d6960ceeeb27eabda4c36799a089
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-20 16:03:15 -0500

    Added problem 87

--- README.md
+++ README.md
@@ -1711,3 +1711,45 @@ For example, given the string "()())()", you should return 1. Given the string "
 
 
 * [Swift](./swift/Problem&#32;86/)
+
+## Problem 87
+
+#### Description
+
+This problem was asked by Uber.
+
+A rule looks like this:
+
+```
+A NE B
+```
+
+This means this means point A is located northeast of point B.
+
+```
+A SW C
+```
+
+means that point A is southwest of C.
+
+Given a list of rules, check if the sum of the rules validate. For example:
+
+```
+A N B
+B NE C
+C N A
+```
+
+does not validate, since A cannot be both north and south of C.
+
+```
+A NW B
+A N B
+```
+
+is considered valid.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;87/)
---
+++ swift/Problem 87/Problem 87Tests/Problem_87Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Problem_87Tests.swift
+//  Problem 87Tests
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-20.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+import XCTest
+@testable import Problem_87
+
+class Problem_87Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
