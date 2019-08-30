5f50db189367e0b37dbb8bb22189be2a73a0ffd1
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-29 20:24:17 -0500

    Added problem 96

---
+++ swift/Problem 96/Problem 96/Problem_96.h
@@ -0,0 +1,19 @@
+//
+//  Problem_96.h
+//  Problem 96
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-29.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_96.
+FOUNDATION_EXPORT double Problem_96VersionNumber;
+
+//! Project version string for Problem_96.
+FOUNDATION_EXPORT const unsigned char Problem_96VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_96/PublicHeader.h>
+
+
---
+++ swift/Problem 96/README.md
@@ -0,0 +1,19 @@
+## Description
+
+This problem was asked by Microsoft.
+
+Given a number in the form of a list of digits, return all possible permutations.
+
+For example, given `[1,2,3]`, return `[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]`.
+
+## Solution
+
+```swift
+// MARK: - TODO
+```
+
+## Test
+
+```swift
+// MARK: - TODO
+```
