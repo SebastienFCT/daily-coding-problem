145796337c99d8b1df9281bf0d91f03910aec7b4
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-08-30 19:52:09 -0500

    Added problem 97

---
+++ swift/Problem 97/Problem 97/Problem_97.h
@@ -0,0 +1,19 @@
+//
+//  Problem_97.h
+//  Problem 97
+//
+//  Created by sebastien FOCK CHOW THO on 2019-08-30.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_97.
+FOUNDATION_EXPORT double Problem_97VersionNumber;
+
+//! Project version string for Problem_97.
+FOUNDATION_EXPORT const unsigned char Problem_97VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_97/PublicHeader.h>
+
+
