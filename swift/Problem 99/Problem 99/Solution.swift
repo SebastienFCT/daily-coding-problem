6bd6adfd94d471e27b4e1a7c7ee6a672d8b93ce9
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-01 21:56:47 -0500

    Solution to problem 99

--- swift/Problem 99/Problem 99/Solution.swift
+++ swift/Problem 99/Problem 99/Solution.swift
@@ -1,16 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 99
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-01.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Microsoft.
+import Foundation
 
-Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
-
-For example, given `[100, 4, 200, 1, 3, 2]`, the longest consecutive element sequence is `[1, 2, 3, 4]`. Return its length: `4`.
-
-Your algorithm should run in `O(n)` complexity.
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     func longestConsecutiveSubsequence() -> Int {
         
@@ -30,17 +27,3 @@ extension Array where Element == Int {
         return references.values.sorted{ $0 > $1 }.first ?? 0
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_99Tests: XCTestCase {
-
-    func test_example() {
-        let input = [100, 4, 200, 1, 3, 2]
-        print(input.longestConsecutiveSubsequence())
-    }
-
-}
-```
