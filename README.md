6303b1cc218fd3f4c9428756987b4711142e736e
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-04 21:20:58 -0500

    Added problem 102

--- README.md
+++ README.md
@@ -2052,3 +2052,18 @@ If a < c OR a==c AND b < d.
 
 
 * [Swift](./swift/Problem&#32;101/)
+
+## Problem 102
+
+#### Description
+
+This problem was asked by Lyft.
+
+Given a list of integers and a number K, return which contiguous elements of the list sum to K.
+
+For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2, 3, 4], since 2 + 3 + 4 = 9.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;102/)
---
+++ swift/Problem 102/Problem 102/Problem_102.h
@@ -0,0 +1,19 @@
+//
+//  Problem_102.h
+//  Problem 102
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-04.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_102.
+FOUNDATION_EXPORT double Problem_102VersionNumber;
+
+//! Project version string for Problem_102.
+FOUNDATION_EXPORT const unsigned char Problem_102VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_102/PublicHeader.h>
+
+
