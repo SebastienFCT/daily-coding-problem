6e6850335c5798538f0c341351540224aef54bb4
SebastienFCT <sebastienfct@gmail.com>
2019-09-07 11:10:44 -0500

    Added problem 105

--- README.md
+++ README.md
@@ -2097,3 +2097,18 @@ For example, `1 -> 4 -> 3 -> 4 -> 1` returns True while `1 -> 4` returns False
 
 
 * [Swift](./swift/Problem&#32;104/)
+
+## Problem 105
+
+#### Description
+
+This problem was asked by Facebook.
+
+Given a function `f`, and `N` return a debounced `f` of `N` milliseconds.
+
+That is, as long as the debounced `f` continues to be invoked, `f` itself will not be called for `N` milliseconds.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;105/)
---
+++ swift/Problem 105/Problem 105/Problem_105.h
@@ -0,0 +1,19 @@
+//
+//  Problem_105.h
+//  Problem 105
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-07.
+//  Copyright © 2019 sebfct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Problem_105.
+FOUNDATION_EXPORT double Problem_105VersionNumber;
+
+//! Project version string for Problem_105.
+FOUNDATION_EXPORT const unsigned char Problem_105VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_105/PublicHeader.h>
+
+
