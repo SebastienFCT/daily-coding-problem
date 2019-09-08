69a6274ca2a7f9653b568052749b8068632a643d
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-08 10:00:29 -0500

    Added problem 106

--- README.md
+++ README.md
@@ -2112,3 +2112,18 @@ That is, as long as the debounced `f` continues to be invoked, `f` itself will n
 
 
 * [Swift](./swift/Problem&#32;105/)
+
+## Problem 106
+
+#### Description
+
+This problem was asked by Pinterest.
+
+Given an integer list where each number represents the number of hops you can make, determine whether you can reach to the last index starting at index 0.
+
+For example, `[2, 0, 1, 0]` returns `True` while `[1, 1, 0, 1]` returns `False`.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;106/)
---
+++ swift/Problem 106/Problem 106/Problem_106.h
@@ -0,0 +1,19 @@
+//
+//  Problem_106.h
+//  Problem 106
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-08.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_106.
+FOUNDATION_EXPORT double Problem_106VersionNumber;
+
+//! Project version string for Problem_106.
+FOUNDATION_EXPORT const unsigned char Problem_106VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_106/PublicHeader.h>
+
+
