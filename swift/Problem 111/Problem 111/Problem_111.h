efb3ed0a0c9d0069f6700fce5f880e248f3b1611
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-13 18:03:33 -0500

    Added problem 111

---
+++ swift/Problem 111/Problem 111/Problem_111.h
@@ -0,0 +1,19 @@
+//
+//  Problem_111.h
+//  Problem 111
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-13.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_111.
+FOUNDATION_EXPORT double Problem_111VersionNumber;
+
+//! Project version string for Problem_111.
+FOUNDATION_EXPORT const unsigned char Problem_111VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_111/PublicHeader.h>
+
+
---
+++ swift/Problem 111/README.md
@@ -0,0 +1,19 @@
+## Description
+
+This problem was asked by Google.
+
+Given a word `W` and a string `S`, find all starting indices in `S` which are anagrams of `W`.
+
+For example, given that `W` is "ab", and `S` is "abxaba", return 0, 3, and 4.
+
+## Solution
+
+```swift
+// MARK: - TODO
+```
+
+## Test
+
+```swift
+// MARK: - TODO
+```
