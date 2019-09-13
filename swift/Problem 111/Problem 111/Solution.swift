913e84cf6b01bf3cbe71eb550400bfbe2d6f5ba1
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-13 18:09:35 -0500

    Solution to problem 111

--- swift/Problem 111/Problem 111/Solution.swift
+++ swift/Problem 111/Problem 111/Solution.swift
@@ -1,14 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 111
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-13.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-Given a word `W` and a string `S`, find all starting indices in `S` which are anagrams of `W`.
-
-For example, given that `W` is "ab", and `S` is "abxaba", return 0, 3, and 4.
-
-## Solution
-
-```swift
 extension String {
     func indexesForAnagrams(of value: String) -> [Int] {
         var result: [Int] = []
@@ -28,16 +27,3 @@ extension String {
         return result
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_111Tests: XCTestCase {
-
-    func test_example() {
-        XCTAssert("abxaba".indexesForAnagrams(of: "ab") == [0, 3, 4])
-    }
-
-}
-```
