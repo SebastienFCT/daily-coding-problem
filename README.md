3aa2730733c8f0195e19cadfa4bc2cac4b53eb56
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-17 17:56:11 -0500

    Added problem 115

--- README.md
+++ README.md
@@ -2265,3 +2265,16 @@ Follow-up: Does your solution work for the following cases: "hello/world:here/",
 
 
 * [Swift](./swift/Problem&#32;114/)
+
+## Problem 115
+
+#### Description
+
+This problem was asked by Google.
+
+Given two non-empty binary trees `s` and `t`, check whether tree `t` has exactly the same structure and node values with a subtree of `s`. A subtree of `s` is a tree consists of a node in `s` and all of this node's descendants. The tree `s` could also be considered as a subtree of itself.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;115/)
---
+++ swift/Problem 115/Problem 115/Problem_115.h
@@ -0,0 +1,19 @@
+//
+//  Problem_115.h
+//  Problem 115
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-17.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_115.
+FOUNDATION_EXPORT double Problem_115VersionNumber;
+
+//! Project version string for Problem_115.
+FOUNDATION_EXPORT const unsigned char Problem_115VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_115/PublicHeader.h>
+
+
