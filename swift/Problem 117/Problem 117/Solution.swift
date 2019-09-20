23a88f05cca570fe1953573e414bd0b2e253d37b
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-19 22:10:19 -0500

    Solution to problem 117

--- swift/Problem 117/Problem 117/Solution.swift
+++ swift/Problem 117/Problem 117/Solution.swift
@@ -1,12 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 117
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-19.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Facebook.
+import Foundation
 
-Given a binary tree, return the level of the tree with minimum sum.
-
-## Solution
-
-```swift
 class Node {
     var value: Int
     var left: Node?
@@ -35,33 +36,3 @@ class Node {
         return candidates.sorted{ $0.sum < $1.sum }.first!
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_117Tests: XCTestCase {
-
-    func test_node_minimum_sum() {
-        let root = Node(value: 0)
-        let l = Node(value: -1)
-        let ll = Node(value: 10)
-        let lr = Node(value: -5)
-        let r = Node(value: 2)
-        let rl = Node(value: 1)
-        let rr = Node(value: -2)
-        
-        root.left = l
-        root.right = r
-        
-        l.left = ll
-        l.right = lr
-        
-        r.left = rl
-        r.right = rr
-        
-        XCTAssert(root.minimumSum(currentLevel: 0, currentSum: root.value) == (level: 2, sum: -6))
-    }
-
-}
-```
--- swift/Problem 117/Problem 117Tests/Problem_117Tests.swift
+++ swift/Problem 117/Problem 117Tests/Problem_117Tests.swift
@@ -11,24 +11,25 @@ import XCTest
 
 class Problem_117Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_node_minimum_sum() {
+        let root = Node(value: 0)
+        let l = Node(value: -1)
+        let ll = Node(value: 10)
+        let lr = Node(value: -5)
+        let r = Node(value: 2)
+        let rl = Node(value: 1)
+        let rr = Node(value: -2)
+        
+        root.left = l
+        root.right = r
+        
+        l.left = ll
+        l.right = lr
+        
+        r.left = rl
+        r.right = rr
+        
+        XCTAssert(root.minimumSum(currentLevel: 0, currentSum: root.value) == (level: 2, sum: -6))
     }
 
 }
