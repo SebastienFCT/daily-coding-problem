c173fe98320b59b1c0b5b3e6a578050511a4530a
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-21 10:25:07 -0500

    Added problem 119

--- README.md
+++ README.md
@@ -2332,3 +2332,18 @@ For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
 
 
 * [Swift](./swift/Problem&#32;117/)
+
+## Problem 119
+
+#### Description
+
+This problem was asked by Google.
+
+Given a set of closed intervals, find the smallest set of numbers that covers all the intervals. If there are multiple smallest sets, return any of them.
+
+For example, given the intervals `[0, 3], [2, 6], [3, 4], [6, 9]`, one set of numbers that covers all these intervals is `{3, 6}`.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;117/)
---
+++ swift/Problem 119/Problem 119/Problem_119.h
@@ -0,0 +1,19 @@
+//
+//  Problem_119.h
+//  Problem 119
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-21.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_119.
+FOUNDATION_EXPORT double Problem_119VersionNumber;
+
+//! Project version string for Problem_119.
+FOUNDATION_EXPORT const unsigned char Problem_119VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_119/PublicHeader.h>
+
+
