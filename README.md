0029d024f373d9ab6cc613c3960b34e85fb76928
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-22 21:29:49 -0500

    Added prolem 120

--- README.md
+++ README.md
@@ -2347,3 +2347,16 @@ For example, given the intervals `[0, 3], [2, 6], [3, 4], [6, 9]`, one set of nu
 
 
 * [Swift](./swift/Problem&#32;117/)
+
+## Problem 120
+
+#### Description
+
+This problem was asked by Microsoft.
+
+Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. And in every even call of `getInstance()`, return the first instance and in every odd call of `getInstance()`, return the second instance.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;120/)
---
+++ swift/Problem 120/Problem 120/Problem_120.h
@@ -0,0 +1,19 @@
+//
+//  Problem_120.h
+//  Problem 120
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-22.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_120.
+FOUNDATION_EXPORT double Problem_120VersionNumber;
+
+//! Project version string for Problem_120.
+FOUNDATION_EXPORT const unsigned char Problem_120VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_120/PublicHeader.h>
+
+
