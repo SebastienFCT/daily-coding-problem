b9f30d95d0bf7183e1c4835fb2e9c295a947986c
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-24 20:36:05 -0500

    Solution to problem 122

--- swift/Problem 122/Problem 122/Solution.swift
+++ swift/Problem 122/Problem 122/Solution.swift
@@ -1,22 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 122
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-24.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This question was asked by Zillow.
+import Foundation
 
-You are given a 2-d `matrix` where each cell represents number of coins in that cell. Assuming we start at `matrix[0][0]`, and can only move right or down, find the maximum number of coins you can collect by the bottom right corner.
-
-For example, in this matrix
-
-```
-0 3 1 1
-2 0 0 4
-1 5 3 1
-```
-
-The most we can collect is 0 + 2 + 1 + 5 + 3 + 1 = 12 coins.
-
-## Solution
-
-```swift
 extension Array where Element == Array<Int> {
     
     typealias Position = (row: Int, column: Int)
@@ -65,22 +56,3 @@ extension Array where Element == Array<Int> {
         return candidates
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_122Tests: XCTestCase {
-
-    func test_example() {
-        let input: [[Int]] = [
-            [0, 3, 1, 1],
-            [2, 0, 0, 4],
-            [1, 5, 3, 1]
-        ]
-        
-        XCTAssert(input.findMaximumPath()?.sum == 12)
-    }
-
-}
-```
--- swift/Problem 122/Problem 122Tests/Problem_122Tests.swift
+++ swift/Problem 122/Problem 122Tests/Problem_122Tests.swift
@@ -11,24 +11,14 @@ import XCTest
 
 class Problem_122Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        let input: [[Int]] = [
+            [0, 3, 1, 1],
+            [2, 0, 0, 4],
+            [1, 5, 3, 1]
+        ]
+        
+        XCTAssert(input.findMaximumPath()?.sum == 12)
     }
 
 }
