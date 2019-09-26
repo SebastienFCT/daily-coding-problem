2afc2661302c9b4fda7ad85ecf91e0278d47c9b7
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-26 18:13:23 -0500

    Added problem 124

---
+++ swift/Problem 124/Problem 124/Problem_124.h
@@ -0,0 +1,19 @@
+//
+//  Problem_124.h
+//  Problem 124
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-26.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_124.
+FOUNDATION_EXPORT double Problem_124VersionNumber;
+
+//! Project version string for Problem_124.
+FOUNDATION_EXPORT const unsigned char Problem_124VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_124/PublicHeader.h>
+
+
