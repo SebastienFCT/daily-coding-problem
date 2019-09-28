372cb9937736ec18b8a2114a9d1d5a96c47ab9e2
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-27 19:13:57 -0500

    Added problem 125

--- README.md
+++ README.md
@@ -2439,3 +2439,28 @@ Write a function that, given `n`, returns the number of rounds you'd expect to p
 
 
 * [Swift](./swift/Problem&#32;124/)
+
+## Problem 125
+
+#### Description
+
+This problem was asked by Google.
+
+Given the root of a binary search tree, and a target `K`, return two nodes in the tree whose sum equals `K`.
+
+For example, given the following tree and `K` of `20`
+
+```
+    10
+   /   \
+ 5      15
+       /  \
+     11    15
+```
+
+Return the nodes `5` and `15`.
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;125/)
---
+++ swift/Problem 125/Problem 125/Problem_125.h
@@ -0,0 +1,19 @@
+//
+//  Problem_125.h
+//  Problem 125
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-27.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_125.
+FOUNDATION_EXPORT double Problem_125VersionNumber;
+
+//! Project version string for Problem_125.
+FOUNDATION_EXPORT const unsigned char Problem_125VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_125/PublicHeader.h>
+
+
