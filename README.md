948bc230192fee43a2799747d465ad715b01b72f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-28 12:44:13 -0500

    Added problem 126

--- README.md
+++ README.md
@@ -2464,3 +2464,16 @@ Return the nodes `5` and `15`.
 
 
 * [Swift](./swift/Problem&#32;125/)
+
+## Problem 126
+
+#### Description
+
+This problem was asked by Facebook.
+
+Write a function that rotates a list by k elements. For example, `[1, 2, 3, 4, 5, 6]` rotated by two becomes `[3, 4, 5, 6, 1, 2]`. Try solving this without creating a copy of the list. How many swap or move operations do you need?
+
+### Solutions
+
+
+* [Swift](./swift/Problem&#32;126/)
---
+++ swift/Problem 126/Problem 126/Problem_126.h
@@ -0,0 +1,19 @@
+//
+//  Problem_126.h
+//  Problem 126
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-28.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_126.
+FOUNDATION_EXPORT double Problem_126VersionNumber;
+
+//! Project version string for Problem_126.
+FOUNDATION_EXPORT const unsigned char Problem_126VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_126/PublicHeader.h>
+
+
