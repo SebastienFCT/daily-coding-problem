c62e7f89a7c7506748dfcfaab3b0a4ab6108f6ce
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-09-29 11:27:37 -0500

    Added problem 127

---
+++ swift/Problem 127/Problem 127/Problem_127.h
@@ -0,0 +1,19 @@
+//
+//  Problem_127.h
+//  Problem 127
+//
+//  Created by sebastien FOCK CHOW THO on 2019-09-29.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_127.
+FOUNDATION_EXPORT double Problem_127VersionNumber;
+
+//! Project version string for Problem_127.
+FOUNDATION_EXPORT const unsigned char Problem_127VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_127/PublicHeader.h>
+
+
