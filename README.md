e65c887fe30886f6e0c6238d65d085d91dc75461
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-07 18:38:34 -0500

    Added problem 135

--- README.md
+++ README.md
@@ -2650,3 +2650,27 @@ Use a more space-efficient data structure, SparseArray, that implements the same
 ### Solutions
 
 * [Swift](./swift/Problem&#32;134/)
+
+## Problem 135
+
+#### Description
+
+This question was asked by Apple.
+
+Given a binary tree, find a minimum path sum from root to a leaf.
+
+For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
+
+```
+  10
+ /  \
+5    5
+ \     \
+   2    1
+       /
+     -1
+```
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;135/)
---
+++ swift/Problem 135/Problem 135/Problem_135.h
@@ -0,0 +1,19 @@
+//
+//  Problem_135.h
+//  Problem 135
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-07.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_135.
+FOUNDATION_EXPORT double Problem_135VersionNumber;
+
+//! Project version string for Problem_135.
+FOUNDATION_EXPORT const unsigned char Problem_135VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_135/PublicHeader.h>
+
+
