619345b58bfd6bf9e8c65843414a9864741063be
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-08 22:35:18 -0500

    Solution to problem 136

--- swift/Problem 136/Problem 136/Solution.swift
+++ swift/Problem 136/Problem 136/Solution.swift
@@ -1,21 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 136
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-08.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This question was asked by Google.
+import Foundation
 
-Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle containing only 1's and return its area.
-
-For example, given the following matrix:
-
-```
-[[1, 0, 0, 0],
- [1, 0, 1, 1],
- [1, 0, 1, 1],
- [0, 1, 0, 0]]
-```
-
-## Solution
-
-```swift
 extension Array where Element == Array<Int> {
     
     func largestIslandSize() -> Int {
@@ -65,23 +57,3 @@ extension Array where Element == Int {
         return result
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_136Tests: XCTestCase {
-
-    func test_example() {
-        let input = [
-            [1, 0, 0, 0],
-            [1, 0, 1, 1],
-            [1, 0, 1, 1],
-            [0, 1, 0, 0]
-        ]
-        
-        XCTAssert(input.largestIslandSize() == 4)
-    }
-
-}
-```
