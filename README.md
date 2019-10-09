e65c4354cda7a8b73e31b66c0742b8220c00a467
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-08 22:03:24 -0500

    Added problem 136

--- README.md
+++ README.md
@@ -2674,3 +2674,26 @@ For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
 ### Solutions
 
 * [Swift](./swift/Problem&#32;135/)
+
+## Problem 136
+
+#### Description
+
+This question was asked by Google.
+
+Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle containing only 1's and return its area.
+
+For example, given the following matrix:
+
+```
+[[1, 0, 0, 0],
+ [1, 0, 1, 1],
+ [1, 0, 1, 1],
+ [0, 1, 0, 0]]
+```
+
+Return 4.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;136/)
---
+++ swift/Problem 136/Problem 136/Problem_136.h
@@ -0,0 +1,19 @@
+//
+//  Problem_136.h
+//  Problem 136
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-08.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_136.
+FOUNDATION_EXPORT double Problem_136VersionNumber;
+
+//! Project version string for Problem_136.
+FOUNDATION_EXPORT const unsigned char Problem_136VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_136/PublicHeader.h>
+
+
