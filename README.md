517f59cab9fb38edc7e6fa808648ab49f83055eb
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-12 22:53:25 -0500

    Added problem 140

--- README.md
+++ README.md
@@ -2760,3 +2760,19 @@ class PeekableInterface(object):
 ### Solutions
 
 * [Swift](./swift/Problem&#32;139/)
+
+## Problem 140
+
+#### Description
+
+This problem was asked by Facebook.
+
+Given an array of integers in which two elements appear exactly once and all other elements appear exactly twice, find the two elements that appear only once.
+
+For example, given the array `[2, 4, 6, 8, 10, 2, 6, 10]`, return `4` and `8`. The order does not matter.
+
+Follow-up: Can you do this in linear time and constant space?
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;140/)
---
+++ swift/Problem 140/Problem 140/Problem_140.h
@@ -0,0 +1,19 @@
+//
+//  Problem_140.h
+//  Problem 140
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-12.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_140.
+FOUNDATION_EXPORT double Problem_140VersionNumber;
+
+//! Project version string for Problem_140.
+FOUNDATION_EXPORT const unsigned char Problem_140VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_140/PublicHeader.h>
+
+
