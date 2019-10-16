073855487f2c86e265e071f0e0f73716bea07e78
SebastienFCT <sebastienfct@gmail.com>
2019-10-15 20:50:48 -0500

    added problem 143

---
+++ swift/Problem 143/Problem 143/Problem_143.h
@@ -0,0 +1,19 @@
+//
+//  Problem_143.h
+//  Problem 143
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-15.
+//  Copyright © 2019 sebfct. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_143.
+FOUNDATION_EXPORT double Problem_143VersionNumber;
+
+//! Project version string for Problem_143.
+FOUNDATION_EXPORT const unsigned char Problem_143VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_143/PublicHeader.h>
+
+
---
+++ swift/Problem 143/Problem 143Tests/Problem_143Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Problem_143Tests.swift
+//  Problem 143Tests
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-15.
+//  Copyright © 2019 sebfct. All rights reserved.
+//
+
+import XCTest
+@testable import Problem_143
+
+class Problem_143Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
