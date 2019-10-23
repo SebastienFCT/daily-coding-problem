1d86441abf183d5a210088142533d506bd3131dc
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-22 21:16:05 -0500

    added problem 150

--- README.md
+++ README.md
@@ -2945,3 +2945,17 @@ You can assume that you can do some pre-processing. `sum()` should be optimized 
 ### Solutions
 
 * [Swift](./swift/Problem&#32;149/)
+
+## Problem 150
+
+#### Description
+
+This problem was asked by LinkedIn.
+
+Given a list of points, a central point, and an integer `k`, find the nearest `k` points from the central point.
+
+For example, given the list of points `[(0, 0), (5, 4), (3, 1)]`, the central point `(1, 2)`, and `k` = 2, return `[(0, 0), (3, 1)]`.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;150/)
---
+++ swift/Problem 150/Problem 150/Problem_150.h
@@ -0,0 +1,19 @@
+//
+//  Problem_150.h
+//  Problem 150
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-22.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_150.
+FOUNDATION_EXPORT double Problem_150VersionNumber;
+
+//! Project version string for Problem_150.
+FOUNDATION_EXPORT const unsigned char Problem_150VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_150/PublicHeader.h>
+
+
