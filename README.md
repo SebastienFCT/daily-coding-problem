21c642418803ed0b49e0c0f690ebabce02fa5a6e
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-27 19:17:36 -0500

    added problem 155

--- README.md
+++ README.md
@@ -3037,3 +3037,20 @@ Recall that a heap has the following operations:
 ### Solutions
 
 * [Swift](./swift/Problem&#32;154/)
+
+## Problem 155
+
+#### Description
+
+This problem was asked by MongoDB.
+
+Given a list of elements, find the majority element, which appears more than half the time (`> floor(len(lst) / 2.0)`).
+
+You can assume that such element exists.
+
+For example, given `[1, 2, 1, 1, 3, 4, 0]`, return 1.
+
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;155/)
---
+++ swift/Problem 155/Problem 155/Problem_155.h
@@ -0,0 +1,19 @@
+//
+//  Problem_155.h
+//  Problem 155
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-27.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_155.
+FOUNDATION_EXPORT double Problem_155VersionNumber;
+
+//! Project version string for Problem_155.
+FOUNDATION_EXPORT const unsigned char Problem_155VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_155/PublicHeader.h>
+
+
