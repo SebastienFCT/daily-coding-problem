8bf75ff4ed0f255034786ddfcc4a5b4a1391c6d2
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-10-30 16:44:27 -0500

    added problem 158

---
+++ swift/Problem 158/Problem 158/Problem_158.h
@@ -0,0 +1,19 @@
+//
+//  Problem_158.h
+//  Problem 158
+//
+//  Created by sebastien FOCK CHOW THO on 2019-10-30.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_158.
+FOUNDATION_EXPORT double Problem_158VersionNumber;
+
+//! Project version string for Problem_158.
+FOUNDATION_EXPORT const unsigned char Problem_158VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_158/PublicHeader.h>
+
+
