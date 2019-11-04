bf23f670da4a837abbfca4b2c96eae2a4ab67ca0
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-11-04 17:16:54 -0600

    solution to problem 163

--- swift/Problem 163/Problem 163/Solution.swift
+++ swift/Problem 163/Problem 163/Solution.swift
@@ -1,18 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 163
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-04.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Jane Street.
+import Foundation
 
-Given an arithmetic expression in [Reverse Polish Notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation), write a program to evaluate it.
-
-The expression is given as a list of numbers and operands. For example: `[5, 3, '+']` should return `5 + 3 = 8`.
-
-For example, `[15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']` should return `5`, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
-
-You can assume the given expression is always valid.
-
-## Solution
-
-```swift
 struct RPN {
     var input: [Any]
     
@@ -47,17 +42,3 @@ struct RPN {
         }
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_163Tests: XCTestCase {
-
-    func test_example() {
-        let rpn = RPN(input: [15, 7, 1, 1, "+", "-", "/", 3, "*", 2, 1, 1, "+", "+", "-"])
-        print(rpn.evaluate())
-    }
-
-}
-```
