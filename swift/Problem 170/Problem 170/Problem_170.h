1ef4705cc1bba0f0fc6f8614724f28141ceff1b5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-11-11 10:01:01 -0600

    added problem 170

---
+++ swift/Problem 170/Problem 170/Problem_170.h
@@ -0,0 +1,19 @@
+//
+//  Problem_170.h
+//  Problem 170
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-11.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_170.
+FOUNDATION_EXPORT double Problem_170VersionNumber;
+
+//! Project version string for Problem_170.
+FOUNDATION_EXPORT const unsigned char Problem_170VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_170/PublicHeader.h>
+
+
