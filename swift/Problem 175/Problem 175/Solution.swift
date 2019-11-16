84a3dfcb3d01bdca9ac93ca566c225f99a44f6ef
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-11-16 10:49:25 -0600

    solution to problem 175

--- swift/Problem 175/Problem 175/Solution.swift
+++ swift/Problem 175/Problem 175/Solution.swift
@@ -1,29 +1,11 @@
-## Description
+//
+//  Solution.swift
+//  Problem 175
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-16.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
-
-You are given a starting state start, a list of transition probabilities for a Markov chain, and a number of steps num_steps. Run the Markov chain starting from start for num_steps and compute the number of times we visited each state.
-
-For example, given the starting state a, number of steps 5000, and the following transition probabilities:
-
-```
-[
-  ('a', 'a', 0.9),
-  ('a', 'b', 0.075),
-  ('a', 'c', 0.025),
-  ('b', 'a', 0.15),
-  ('b', 'b', 0.8),
-  ('b', 'c', 0.05),
-  ('c', 'a', 0.25),
-  ('c', 'b', 0.25),
-  ('c', 'c', 0.5)
-]
-```
-
-One instance of running this Markov chain might produce `{ 'a': 3012, 'b': 1656, 'c': 332 }`.
-## Solution
-
-```swift
 import Foundation
 
 typealias MarkovState = (from: String, to: String, probability: Double)
@@ -71,28 +53,3 @@ extension Array where Element == MarkovState {
         return nil
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_175Tests: XCTestCase {
-
-    func test_example() {
-        let input: [MarkovState] = [
-          ("a", "a", 0.9),
-          ("a", "b", 0.075),
-          ("a", "c", 0.025),
-          ("b", "a", 0.15),
-          ("b", "b", 0.8),
-          ("b", "c", 0.05),
-          ("c", "a", 0.25),
-          ("c", "b", 0.25),
-          ("c", "c", 0.5)
-        ]
-        
-        print(input.compute(start: "a", steps: 5000))
-    }
-
-}
-```
--- swift/Problem 175/Problem 175Tests/Problem_175Tests.swift
+++ swift/Problem 175/Problem 175Tests/Problem_175Tests.swift
@@ -11,24 +11,20 @@ import XCTest
 
 class Problem_175Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        let input: [MarkovState] = [
+          ("a", "a", 0.9),
+          ("a", "b", 0.075),
+          ("a", "c", 0.025),
+          ("b", "a", 0.15),
+          ("b", "b", 0.8),
+          ("b", "c", 0.05),
+          ("c", "a", 0.25),
+          ("c", "b", 0.25),
+          ("c", "c", 0.5)
+        ]
+        
+        print(input.compute(start: "a", steps: 5000))
     }
 
 }
