e4938386aafa8ea76b32809693cb81139329b7bb
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-11-20 18:47:52 -0600

    solution to problem 179

--- swift/Problem 179/Problem 179/Solution.swift
+++ swift/Problem 179/Problem 179/Solution.swift
@@ -1,23 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 179
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-20.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Two Sigma.
-This problem was asked by Google.
+import Foundation
 
-Given the sequence of keys visited by a postorder traversal of a binary search tree, reconstruct the tree.
-
-For example, given the sequence `2, 4, 3, 8, 7, 5`, you should construct the following tree:
-
-```
-    5
-   / \
-  3   7
- / \   \
-2   4   8
-```
-
-## Solution
-
-```swift
 class Node {
     var value: Int
     var left: Node?
@@ -77,24 +67,3 @@ class Node {
     }
 
 }
-```
-
-## Test
-
-```swift
-class Problem_179Tests: XCTestCase {
-
-    func test_example() {
-        let root = Node.buildTree(postorderedTraversedKeys: [2,4,3,8,7,5])
-        
-        XCTAssert(root?.value == 5)
-        XCTAssert(root?.left?.value == 3)
-        XCTAssert(root?.left?.left?.value == 2)
-        XCTAssert(root?.left?.right?.value == 4)
-        XCTAssert(root?.right?.value == 7)
-        XCTAssertNil(root?.right?.left?.value)
-        XCTAssert(root?.right?.right?.value == 8)
-    }
-
-}
-```
--- swift/Problem 179/Problem 179Tests/Problem_179Tests.swift
+++ swift/Problem 179/Problem 179Tests/Problem_179Tests.swift
@@ -11,24 +11,16 @@ import XCTest
 
 class Problem_179Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        let root = Node.buildTree(postorderedTraversedKeys: [2,4,3,8,7,5])
+        
+        XCTAssert(root?.value == 5)
+        XCTAssert(root?.left?.value == 3)
+        XCTAssert(root?.left?.left?.value == 2)
+        XCTAssert(root?.left?.right?.value == 4)
+        XCTAssert(root?.right?.value == 7)
+        XCTAssertNil(root?.right?.left?.value)
+        XCTAssert(root?.right?.right?.value == 8)
     }
 
 }
