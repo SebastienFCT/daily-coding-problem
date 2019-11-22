8ec756f339e2f2a7b6f0187544e9341dd660a3a5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-11-22 07:51:14 -0600

    added problem 181

--- README.md
+++ README.md
@@ -3557,3 +3557,19 @@ Hint: Try working backwards from the end state.
 ### Solutions
 
 * [Swift](./swift/Problem&#32;180/)
+
+## Problem 181
+
+#### Description
+
+This problem was asked by Google.
+
+Given a string, split it into as few strings as possible such that each string is a palindrome.
+
+For example, given the input string racecarannakayak, return ["racecar", "anna", "kayak"].
+
+Given the input string abc, return ["a", "b", "c"].
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;181/)
---
+++ swift/Problem 181/Problem 181/Problem_181.h
@@ -0,0 +1,19 @@
+//
+//  Problem_181.h
+//  Problem 181
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-22.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_181.
+FOUNDATION_EXPORT double Problem_181VersionNumber;
+
+//! Project version string for Problem_181.
+FOUNDATION_EXPORT const unsigned char Problem_181VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_181/PublicHeader.h>
+
+
