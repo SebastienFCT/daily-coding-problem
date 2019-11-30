15546e4e8598cbb0b8f516cdc428a70d3f2dcf22
SebastienFCT <sebastienfct@gmail.com>
2019-11-30 11:25:18 -0600

    added problem 189

--- README.md
+++ README.md
@@ -3722,3 +3722,17 @@ How can we make it print out what we apparently want?
 ### Solutions
 
 * [Swift](./others/Problem&#32;188/)
+
+## Problem 189
+
+#### Description
+
+This problem was asked by Google.
+
+Given an array of elements, return the length of the longest subarray where all its elements are distinct.
+
+For example, given the array `[5, 1, 3, 5, 2, 3, 4, 1]`, return 5 as the longest subarray of distinct elements is `[5, 2, 3, 4, 1]`.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;189/)
---
+++ swift/Problem 189/Problem 189/Problem_189.h
@@ -0,0 +1,19 @@
+//
+//  Problem_189.h
+//  Problem 189
+//
+//  Created by sebastien FOCK CHOW THO on 2019-11-30.
+//  Copyright © 2019 sebfct. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Problem_189.
+FOUNDATION_EXPORT double Problem_189VersionNumber;
+
+//! Project version string for Problem_189.
+FOUNDATION_EXPORT const unsigned char Problem_189VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_189/PublicHeader.h>
+
+
