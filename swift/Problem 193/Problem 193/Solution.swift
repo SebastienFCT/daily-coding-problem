6b5fe79f2c32fa788caea1d9d48f2d6b2d9f580c
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-04 18:13:11 -0600

    solution to problem 193

--- swift/Problem 193/Problem 193/Solution.swift
+++ swift/Problem 193/Problem 193/Solution.swift
@@ -1,16 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 193
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-04.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Affirm.
+import Foundation
 
-Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock. You're also given a number fee that represents a transaction fee for each buy and sell transaction.
-
-You must buy before you can sell the stock, but you can make as many transactions as you like.
-
-For example, given [1, 3, 2, 8, 4, 10] and fee = 2, you should return 9, since you could buy the stock at 1 dollar, and sell at 8 dollars, and then buy it at 4 dollars and sell it at 10 dollars. Since we did two transactions, there is a 4 dollar fee, so we have 7 + 6 = 13 profit minus 4 dollars of fees.
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     
     typealias TransactionState = (holdingStock: Bool, profits: Int)
@@ -48,16 +45,3 @@ extension Array where Element == Int {
         return result
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_193Tests: XCTestCase {
-
-    func test_example() {
-        XCTAssert([1, 3, 2, 8, 4, 10].optimizeTransactions(withSellingFees: 2) == 9)
-    }
-
-}
-```
