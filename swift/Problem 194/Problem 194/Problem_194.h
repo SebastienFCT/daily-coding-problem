32d4c3a1f21d73e0acf34c9a3f0e05385cd407a9
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-05 19:08:49 -0600

    added problem 194

---
+++ swift/Problem 194/Problem 194/Problem_194.h
@@ -0,0 +1,19 @@
+//
+//  Problem_194.h
+//  Problem 194
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-05.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_194.
+FOUNDATION_EXPORT double Problem_194VersionNumber;
+
+//! Project version string for Problem_194.
+FOUNDATION_EXPORT const unsigned char Problem_194VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_194/PublicHeader.h>
+
+
