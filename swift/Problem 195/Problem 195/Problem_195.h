335bc5bc72c9273c89a21461923a4479cedcefbf
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-06 09:30:22 -0600

    added problem 195

---
+++ swift/Problem 195/Problem 195/Problem_195.h
@@ -0,0 +1,19 @@
+//
+//  Problem_195.h
+//  Problem 195
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-06.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_195.
+FOUNDATION_EXPORT double Problem_195VersionNumber;
+
+//! Project version string for Problem_195.
+FOUNDATION_EXPORT const unsigned char Problem_195VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_195/PublicHeader.h>
+
+
