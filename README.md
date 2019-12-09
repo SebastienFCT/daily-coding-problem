0a26b67fbd5e55da2b813d8c0b88d5feb630e2f0
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-09 17:57:20 -0600

    added problem 198

--- README.md
+++ README.md
@@ -3874,3 +3874,18 @@ Given an array and a number k that's smaller than the length of the array, rotat
 ### Solutions
 
 * [Swift](./swift/Problem&#32;197/)
+
+
+## Problem 198
+
+#### Description
+
+This problem was asked by Google.
+
+Given a set of distinct positive integers, find the largest subset such that every pair of elements in the subset (i, j) satisfies either i % j = 0 or j % i = 0.
+
+For example, given the set [3, 5, 10, 20, 21], you should return [5, 10, 20]. Given [1, 3, 6, 24], return [1, 3, 6, 24].
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;198/)
---
+++ swift/Problem 198/Problem 198/Problem_198.h
@@ -0,0 +1,19 @@
+//
+//  Problem_198.h
+//  Problem 198
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-09.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_198.
+FOUNDATION_EXPORT double Problem_198VersionNumber;
+
+//! Project version string for Problem_198.
+FOUNDATION_EXPORT const unsigned char Problem_198VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_198/PublicHeader.h>
+
+
