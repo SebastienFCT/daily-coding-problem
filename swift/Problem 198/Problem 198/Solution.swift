033b526bbe7f08ff195e08aab8fc3fc8dfea9f63
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-09 18:08:49 -0600

    solution to problem 198

--- swift/Problem 198/Problem 198/Solution.swift
+++ swift/Problem 198/Problem 198/Solution.swift
@@ -1,14 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 198
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-09.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-Given a set of distinct positive integers, find the largest subset such that every pair of elements in the subset (i, j) satisfies either i % j = 0 or j % i = 0.
-
-For example, given the set [3, 5, 10, 20, 21], you should return [5, 10, 20]. Given [1, 3, 6, 24], return [1, 3, 6, 24].
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     
     /// find the largest subset such that every pair of elements in the subset (i, j) satisfies either i % j = 0 or j % i = 0.
@@ -56,20 +55,3 @@ extension Array where Element == Int {
         return true
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_198Tests: XCTestCase {
-
-    func test_example_1() {
-        XCTAssert([3, 5, 10, 20, 21].largestSetWherePairsAreMultiple() == [5, 10, 20])
-    }
-    
-    func test_example_2() {
-        XCTAssert([1, 3, 6, 24].largestSetWherePairsAreMultiple() == [1, 3, 6, 24])
-    }
-
-}
-```
--- swift/Problem 198/Problem 198Tests/Problem_198Tests.swift
+++ swift/Problem 198/Problem 198Tests/Problem_198Tests.swift
@@ -11,24 +11,12 @@ import XCTest
 
 class Problem_198Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_example_1() {
+        XCTAssert([3, 5, 10, 20, 21].largestSetWherePairsAreMultiple() == [5, 10, 20])
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_example_2() {
+        XCTAssert([1, 3, 6, 24].largestSetWherePairsAreMultiple() == [1, 3, 6, 24])
     }
 
 }
