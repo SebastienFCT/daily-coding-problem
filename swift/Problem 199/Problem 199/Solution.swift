d8417c29f48fbb2fa9e08c5d5f68a9d490480b0f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-10 19:55:49 -0600

    solution to problem 199

--- swift/Problem 199/Problem 199/Solution.swift
+++ swift/Problem 199/Problem 199/Solution.swift
@@ -1,15 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 199
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-10.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Facebook.
+import Foundation
 
-Given a string of parentheses, find the balanced string that can be produced from it using the minimum number of insertions and deletions. If there are multiple solutions, return any of them.
-
-For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".
-
-
-## Solution
-
-```swift
 extension String {
     
     func closerBalancedParentheses() -> String {
@@ -61,20 +59,3 @@ extension String {
         return (copy, currentModifications)
     }
 }
-```
-
-## Test
-
-```swift
-class Problem_199Tests: XCTestCase {
-
-    func test_example_1() {
-        XCTAssert("(()".closerBalancedParentheses() == "(())")
-    }
-    
-    func test_example_2() {
-        XCTAssert("))()(".closerBalancedParentheses() == "()()()()")
-    }
-
-}
-```
