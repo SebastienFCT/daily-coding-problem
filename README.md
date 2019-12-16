ec39d53d4633877ece440f1bc2ef864be31f5665
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-15 21:03:18 -0600

    added problem 204

--- README.md
+++ README.md
@@ -3965,3 +3965,15 @@ For example, given [5, 7, 10, 3, 4], return 3.
 ### Solutions
 
 * [Swift](./swift/Problem&#32;203/)
+
+## Problem 204
+
+#### Description
+
+This problem was asked by Amazon.
+
+Given a complete binary tree, count the number of nodes in faster than O(n) time. Recall that a complete binary tree has every level filled except the last, and the nodes in the last level are filled starting from the left.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;204/)
---
+++ swift/Problem 204/Problem 204/Problem_204.h
@@ -0,0 +1,19 @@
+//
+//  Problem_204.h
+//  Problem 204
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-15.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_204.
+FOUNDATION_EXPORT double Problem_204VersionNumber;
+
+//! Project version string for Problem_204.
+FOUNDATION_EXPORT const unsigned char Problem_204VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_204/PublicHeader.h>
+
+
