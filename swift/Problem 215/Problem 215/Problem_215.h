c2420b699944089a6f2551911685f0612b569cbb
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-26 22:39:36 -0600

    added problem 215

---
+++ swift/Problem 215/Problem 215/Problem_215.h
@@ -0,0 +1,19 @@
+//
+//  Problem_215.h
+//  Problem 215
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-26.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_215.
+FOUNDATION_EXPORT double Problem_215VersionNumber;
+
+//! Project version string for Problem_215.
+FOUNDATION_EXPORT const unsigned char Problem_215VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_215/PublicHeader.h>
+
+
