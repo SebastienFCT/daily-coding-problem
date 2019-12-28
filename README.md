2676bfe822bad389d053b4abb983902ef5ecbeda
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2019-12-28 16:02:37 -0600

    added problem 217

--- README.md
+++ README.md
@@ -4181,3 +4181,15 @@ For the input `XIV`, for instance, you should return `14`.
 ### Solutions
 
 * [Swift](./swift/Problem&#32;216/)
+
+## Problem 217
+
+#### Description
+
+We say a number is sparse if there are no adjacent ones in its binary representation. For example, `21` (10101) is sparse, but `22` (10110) is not. For a given input `N`, find the smallest sparse number greater than or equal to `N`.
+
+Do this in faster than `O(N log N)` time.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;217/)
---
+++ swift/Problem 217/Problem 217/Problem_217.h
@@ -0,0 +1,19 @@
+//
+//  Problem_217.h
+//  Problem 217
+//
+//  Created by sebastien FOCK CHOW THO on 2019-12-28.
+//  Copyright © 2019 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_217.
+FOUNDATION_EXPORT double Problem_217VersionNumber;
+
+//! Project version string for Problem_217.
+FOUNDATION_EXPORT const unsigned char Problem_217VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_217/PublicHeader.h>
+
+
