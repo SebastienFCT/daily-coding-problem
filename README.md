3fecaeb2824160b4a9fcd870cf5f39657d552ea6
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-01-01 18:44:54 -0600

    added problem 221

--- README.md
+++ README.md
@@ -4236,3 +4236,16 @@ Write a program that returns the maximum amount of money you can win with certai
 ### Solutions
 
 * [Swift](./swift/Problem&#32;220/)
+
+## Problem 221
+
+#### Description
+
+This problem was asked by Zillow.
+
+Let's define a "sevenish" number to be one which is either a power of `7`, or the sum of unique powers of `7`. The first few sevenish numbers are `1`, `7`, `8`, `49`, and so on. Create an algorithm to find the `n`th sevenish number.
+
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;221/)
---
+++ swift/Problem 221/Problem 221/Problem_221.h
@@ -0,0 +1,19 @@
+//
+//  Problem_221.h
+//  Problem 221
+//
+//  Created by sebastien FOCK CHOW THO on 2020-01-01.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_221.
+FOUNDATION_EXPORT double Problem_221VersionNumber;
+
+//! Project version string for Problem_221.
+FOUNDATION_EXPORT const unsigned char Problem_221VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_221/PublicHeader.h>
+
+
