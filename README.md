646e983e6f82339cec974020ec0d8e35dcd2dbc5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-01-07 18:12:35 -0600

    added problem 227

--- README.md
+++ README.md
@@ -4328,3 +4328,16 @@ For example, given `['xww', 'wxyz', 'wxyw', 'ywx', 'ywz']`, you should return `[
 ### Solutions
 
 * [Solution](./swift/Problem&#32;226/)
+
+## Problem 227
+
+#### Description
+
+This problem was asked by Facebook.
+
+Boggle is a game played on a `4 x 4` grid of letters. The goal is to find as many words as possible that can be formed by a sequence of adjacent letters in the grid, using each cell at most once. Given a game board and a dictionary of valid words, implement a Boggle solver.
+
+
+### Solutions
+
+* [Solution](./swift/Problem&#32;227/)
---
+++ swift/Problem 227/Problem 227/Problem_227.h
@@ -0,0 +1,19 @@
+//
+//  Problem_227.h
+//  Problem 227
+//
+//  Created by sebastien FOCK CHOW THO on 2020-01-07.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_227.
+FOUNDATION_EXPORT double Problem_227VersionNumber;
+
+//! Project version string for Problem_227.
+FOUNDATION_EXPORT const unsigned char Problem_227VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_227/PublicHeader.h>
+
+
