17644b812b5a95415a4e66faf6c71c97347665a5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-01-10 20:40:57 -0600

    added problem 230

---
+++ swift/Problem 230/Problem 230/Problem_230.h
@@ -0,0 +1,19 @@
+//
+//  Problem_230.h
+//  Problem 230
+//
+//  Created by sebastien FOCK CHOW THO on 2020-01-10.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_230.
+FOUNDATION_EXPORT double Problem_230VersionNumber;
+
+//! Project version string for Problem_230.
+FOUNDATION_EXPORT const unsigned char Problem_230VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_230/PublicHeader.h>
+
+
