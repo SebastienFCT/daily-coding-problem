bd294b107a6d21ecae633d6cc9ae3ee13d96b86d
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-01-10 19:03:41 -0600

    added problem 1c

---
+++ swift/Problem 1c/Problem 1c/Problem_1c.h
@@ -0,0 +1,19 @@
+//
+//  Problem_1c.h
+//  Problem 1c
+//
+//  Created by sebastien FOCK CHOW THO on 2020-01-10.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_1c.
+FOUNDATION_EXPORT double Problem_1cVersionNumber;
+
+//! Project version string for Problem_1c.
+FOUNDATION_EXPORT const unsigned char Problem_1cVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_1c/PublicHeader.h>
+
+
