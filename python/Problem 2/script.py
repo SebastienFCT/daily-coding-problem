5de74bda7814021841a79265384660ec0457f5e8
Shujun HUANG <huang.shujun43@gmail.com>
2020-01-11 11:54:53 -0600

    Update script.py

--- python/Problem 2/script.py
+++ python/Problem 2/script.py
@@ -1,25 +1,33 @@
-import numpy as np
+from numpy import prod
 def ProdOfArrayExceptOne(array):
     res = []
     for index in range(len(array)):
-        res.append(np.prod(np.delete(array, index)))
+        res.append(prod(np.delete(array, index)))
     return(res)
 
 
-import numpy as np
+from numpy import prod
 def ProdOfArrayExceptOne(array):
     res = []
     for index in range(len(array)):
-        res.append(np.prod(array[:index] + array[(index + 1):]))
+        res.append(prod(array[:index] + array[(index + 1):]))
     return(res)
-
 
 from functools import reduce
 def ProdOfArrayExceptOne(array):
     res = []
     for index in range(len(array)):
         res.append(reduce(lambda x, y: x*y, array[:index] + array[(index + 1):]))
-    return res
+    return(res)
+
+
+from functools import reduce
+def ProdOfArrayExceptOne(array):
+    total = reduce(lambda x, y: x*y, array)
+    res = []
+    for item in array:
+        res.append(total/item)
+    return(res)
 
 assert ProdOfArrayExceptOne([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
 assert ProdOfArrayExceptOne([3, 2, 1]) == [2, 3, 6]
