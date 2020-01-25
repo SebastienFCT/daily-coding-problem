c8a60b7e395485bcf62df15ecf64a65150e3abac
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-01-25 09:17:00 -0600

    solution to problem 245

--- swift/Problem 245/Problem 245/Solution.swift
+++ swift/Problem 245/Problem 245/Solution.swift
@@ -1,14 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 245
+//
+//  Created by sebastien FOCK CHOW THO on 2020-01-25.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Yelp.
+import Foundation
 
-You are given an array of integers, where each element represents the maximum number of steps that can be jumped going forward from that element. Write a function to return the minimum number of jumps you must take in order to get from the start to the end of the array.
-
-For example, given `[6, 2, 4, 0, 5, 1, 1, 4, 2, 9]`, you should return `2`, as the optimal solution involves jumping from `6` to `5`, and then from `5` to `9`.
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     
     func minJumps() -> Int {
@@ -48,21 +47,3 @@ extension Array where Element == Int {
         return result
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_245Tests: XCTestCase {
-
-    func test_example() {
-        
-        let input = [6, 2, 4, 0, 5, 1, 1, 4, 2, 9]
-        let expected = 2
-        let actual = input.minJumps()
-        
-        XCTAssert(actual == expected)
-    }
-
-}
-```
--- swift/Problem 245/Problem 245Tests/Problem_245Tests.swift
+++ swift/Problem 245/Problem 245Tests/Problem_245Tests.swift
@@ -11,24 +11,13 @@ import XCTest
 
 class Problem_245Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        
+        let input = [6, 2, 4, 0, 5, 1, 1, 4, 2, 9]
+        let expected = 2
+        let actual = input.minJumps()
+        
+        XCTAssert(actual == expected)
     }
 
 }
