a131b0f52e38a3328394f1a1deb635e94f91cb6f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-05 18:39:31 -0600

    added problem 256

--- README.md
+++ README.md
@@ -4872,3 +4872,15 @@ Given a graph, find its transitive closure.
 ### Solutions
 
 * [Solution](./swift/Problem&#32;255/)
+
+## Problem 256
+
+#### Description
+
+This problem was asked by Fitbit.
+
+Given a linked list, rearrange the node values such that they appear in alternating `low -> high -> low -> high` ... form. For example, given `1 -> 2 -> 3 -> 4 -> 5`, you should return `1 -> 3 -> 2 -> 5 -> 4`.
+
+### Solutions
+
+* [Solution](./swift/Problem&#32;256/)
---
+++ swift/Problem 256/Problem 256/Problem_256.h
@@ -0,0 +1,19 @@
+//
+//  Problem_256.h
+//  Problem 256
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-05.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_256.
+FOUNDATION_EXPORT double Problem_256VersionNumber;
+
+//! Project version string for Problem_256.
+FOUNDATION_EXPORT const unsigned char Problem_256VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_256/PublicHeader.h>
+
+
