7b9f45f14a6795f5279bb10efe0b794f5b565d2f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-10 20:55:36 -0600

    added problem 261

---
+++ swift/Problem 261/Problem 261/Problem_261.h
@@ -0,0 +1,19 @@
+//
+//  Problem_261.h
+//  Problem 261
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-10.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_261.
+FOUNDATION_EXPORT double Problem_261VersionNumber;
+
+//! Project version string for Problem_261.
+FOUNDATION_EXPORT const unsigned char Problem_261VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_261/PublicHeader.h>
+
+
