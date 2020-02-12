1c7b03ba9fe599602c3a295253eda5f4c0bbe34f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-11 18:43:20 -0600

    solution to problem 262

--- swift/Problem 262/Problem 262/Solution.swift
+++ swift/Problem 262/Problem 262/Solution.swift
@@ -1,12 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 262
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-11.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Mozilla.
+import Foundation
 
-A bridge in a connected (undirected) graph is an edge that, if removed, causes the graph to become disconnected. Find all the bridges in a graph.
-
-## Solution
-
-```swift
 class Node: Equatable {
     
     // To make it simpler, we assume node values are unique
@@ -94,64 +95,3 @@ struct Graph {
         return result
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_262Tests: XCTestCase {
-
-    func test_graph_bridges() {
-        
-        let a = Node(value: 1)
-        let b = Node(value: 2)
-        let c = Node(value: 3)
-        let d = Node(value: 4)
-        let e = Node(value: 5)
-        
-        let input = Graph(
-            nodes: [a, b, c, d, e],
-            edges: [
-                Edge(left: a, right: b),
-                Edge(left: b, right: c),
-                Edge(left: c, right: d),
-                Edge(left: d, right: e),
-                Edge(left: e, right: a)
-            ]
-        )
-        
-        let actual = input.findBridges()
-        
-        XCTAssert(actual.count == 5)
-        
-        for edge in actual {
-            print(edge.toString())
-        }
-    }
-    
-    func test_graph_bridges_2() {
-        
-        let a = Node(value: 1)
-        let b = Node(value: 2)
-        let c = Node(value: 3)
-        
-        let input = Graph(
-            nodes: [a, b, c],
-            edges: [
-                Edge(left: a, right: b),
-                Edge(left: a, right: c),
-                Edge(left: b, right: a),
-                Edge(left: b, right: c),
-                Edge(left: c, right: a),
-                Edge(left: c, right: b)
-            ]
-        )
-        
-        let actual = input.findBridges()
-        
-        XCTAssert(actual.count == 0)
-        
-    }
-
-}
-```
--- swift/Problem 262/Problem 262Tests/Problem_262Tests.swift
+++ swift/Problem 262/Problem 262Tests/Problem_262Tests.swift
@@ -11,24 +11,56 @@ import XCTest
 
 class Problem_262Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_graph_bridges() {
+        
+        let a = Node(value: 1)
+        let b = Node(value: 2)
+        let c = Node(value: 3)
+        let d = Node(value: 4)
+        let e = Node(value: 5)
+        
+        let input = Graph(
+            nodes: [a, b, c, d, e],
+            edges: [
+                Edge(left: a, right: b),
+                Edge(left: b, right: c),
+                Edge(left: c, right: d),
+                Edge(left: d, right: e),
+                Edge(left: e, right: a)
+            ]
+        )
+        
+        let actual = input.findBridges()
+        
+        XCTAssert(actual.count == 5)
+        
+        for edge in actual {
+            print(edge.toString())
         }
+    }
+    
+    func test_graph_bridges_2() {
+        
+        let a = Node(value: 1)
+        let b = Node(value: 2)
+        let c = Node(value: 3)
+        
+        let input = Graph(
+            nodes: [a, b, c],
+            edges: [
+                Edge(left: a, right: b),
+                Edge(left: a, right: c),
+                Edge(left: b, right: a),
+                Edge(left: b, right: c),
+                Edge(left: c, right: a),
+                Edge(left: c, right: b)
+            ]
+        )
+        
+        let actual = input.findBridges()
+        
+        XCTAssert(actual.count == 0)
+        
     }
 
 }
