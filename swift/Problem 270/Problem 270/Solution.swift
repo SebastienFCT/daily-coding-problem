446fde204af40e338bb250da26ce89b093fc12c4
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-19 21:34:31 -0600

    solution for problem 270

--- swift/Problem 270/Problem 270/Solution.swift
+++ swift/Problem 270/Problem 270/Solution.swift
@@ -1,31 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 270
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-19.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Twitter.
+import Foundation
 
-A network consists of nodes labeled `0` to `N`. You are given a list of edges `(a, b, t)`, describing the time `t` it takes for a message to be sent from node a to node b. Whenever a node receives a message, it immediately passes the message on to a neighboring node, if possible.
-
-Assuming all nodes are connected, determine how long it will take for every node to receive a message that begins at node 0.
-
-For example, given `N = 5`, and the following edges:
-
-```
-edges = [
-    (0, 1, 5),
-    (0, 2, 3),
-    (0, 5, 4),
-    (1, 3, 8),
-    (2, 3, 1),
-    (3, 5, 10),
-    (3, 4, 5)
-]
-```
-
-You should return `9`, because propagating the message from `0 -> 2 -> 3 -> 4` will take that much time.
-
-
-## Solution
-
-```swift
 typealias Edge = (from: Int, to: Int, time: Int)
 
 struct Network {
@@ -78,32 +60,3 @@ struct Network {
         
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_270Tests: XCTestCase {
-
-    func test_example() {
-        
-        let edges: [Edge] = [
-            (0, 1, 5),
-            (0, 2, 3),
-            (0, 5, 4),
-            (1, 3, 8),
-            (2, 3, 1),
-            (3, 5, 10),
-            (3, 4, 5)
-        ]
-        
-        let network = Network(length: 5)
-        
-        let actual = network.shortestPropagationTime(forEdges: edges)
-        let expected = 9
-        
-        XCTAssert(actual == expected)
-    }
-
-}
-```
--- swift/Problem 270/Problem 270Tests/Problem_270Tests.swift
+++ swift/Problem 270/Problem 270Tests/Problem_270Tests.swift
@@ -11,24 +11,24 @@ import XCTest
 
 class Problem_270Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        
+        let edges: [Edge] = [
+            (0, 1, 5),
+            (0, 2, 3),
+            (0, 5, 4),
+            (1, 3, 8),
+            (2, 3, 1),
+            (3, 5, 10),
+            (3, 4, 5)
+        ]
+        
+        let network = Network(length: 5)
+        
+        let actual = network.shortestPropagationTime(forEdges: edges)
+        let expected = 9
+        
+        XCTAssert(actual == expected)
     }
 
 }
