912eb0a7f5d736cd8924f4f569a35b8aba132c62
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-28 22:34:18 -0600

    solution to problem 279

--- swift/Problem 279/Problem 279/Solution.swift
+++ swift/Problem 279/Problem 279/Solution.swift
@@ -1,26 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 279
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-28.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Twitter.
+import Foundation
 
-A classroom consists of `N` students, whose friendships can be represented in an adjacency list. For example, the following descibes a situation where `0` is friends with `1` and `2`, `3` is friends with `6`, and so on.
-
-```
-{0: [1, 2],
- 1: [0, 5],
- 2: [0],
- 3: [6],
- 4: [],
- 5: [1],
- 6: [3]} 
-```
-Each student can be placed in a friend group, which can be defined as the transitive closure of that student's friendship relations. In other words, this is the smallest set such that no student in the group has any friends outside this group. For the example above, the friend groups would be `{0, 1, 2, 5}`, `{3, 6}`, `{4}`.
-
-Given a friendship list such as the one above, determine the number of friend groups in the class.
-
-
-## Solution
-
-```swift
 typealias Friendlist = (person: Int, friends: [Int])
 
 extension Array where Element == Friendlist {
@@ -68,27 +55,3 @@ extension Array where Element == Friendlist {
         return result
     }
 }
-
-```
-
-## Tests
-
-```swift
-class Problem_279Tests: XCTestCase {
-
-    func test_example() {
-        let input: [Friendlist] = [
-            (0, [1, 2]),
-            (1, [0, 5]),
-            (2, [0]),
-            (3, [6]),
-            (4, []),
-            (5, [1]),
-            (6, [3])
-        ]
-        
-        XCTAssert(input.friendGroups().count == 3)
-    }
-
-}
-```
--- swift/Problem 279/Problem 279Tests/Problem_279Tests.swift
+++ swift/Problem 279/Problem 279Tests/Problem_279Tests.swift
@@ -11,24 +11,18 @@ import XCTest
 
 class Problem_279Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        let input: [Friendlist] = [
+            (0, [1, 2]),
+            (1, [0, 5]),
+            (2, [0]),
+            (3, [6]),
+            (4, []),
+            (5, [1]),
+            (6, [3])
+        ]
+        
+        XCTAssert(input.friendGroups().count == 3)
     }
 
 }
