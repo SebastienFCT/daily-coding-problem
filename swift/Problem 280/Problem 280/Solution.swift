619a9dd3e673d8a97d69811038a929e1e383a00e
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-02-29 16:44:51 -0600

    solution to problem 280

--- swift/Problem 280/Problem 280/Solution.swift
+++ swift/Problem 280/Problem 280/Solution.swift
@@ -1,13 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 280
+//
+//  Created by sebastien FOCK CHOW THO on 2020-02-29.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Pandora.
+import Foundation
 
-Given an undirected graph, determine if it contains a cycle.
-
-
-## Solution
-
-```swift
 class Node {
     var value: Int
     var nodes: [Node]
@@ -44,34 +44,3 @@ class Node {
         return false
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_280Tests: XCTestCase {
-
-    func test_graph_cycle() {
-        
-        let a = Node(value: 0)
-        let b = Node(value: 1)
-        let c = Node(value: 2)
-        
-        a.nodes = [b]
-        b.nodes = [c]
-        c.nodes = [a]
-        
-        XCTAssertTrue(a.containsCycle(current: []))
-        
-        let a2 = Node(value: 0)
-        let b2 = Node(value: 1)
-        let c2 = Node(value: 2)
-        
-        a2.nodes = [b2]
-        b2.nodes = [c2]
-        
-        XCTAssertFalse(a2.containsCycle(current: []))
-    }
-
-}
-```
--- swift/Problem 280/Problem 280Tests/Problem_280Tests.swift
+++ swift/Problem 280/Problem 280Tests/Problem_280Tests.swift
@@ -11,24 +11,26 @@ import XCTest
 
 class Problem_280Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_graph_cycle() {
+        
+        let a = Node(value: 0)
+        let b = Node(value: 1)
+        let c = Node(value: 2)
+        
+        a.nodes = [b]
+        b.nodes = [c]
+        c.nodes = [a]
+        
+        XCTAssertTrue(a.containsCycle(current: []))
+        
+        let a2 = Node(value: 0)
+        let b2 = Node(value: 1)
+        let c2 = Node(value: 2)
+        
+        a2.nodes = [b2]
+        b2.nodes = [c2]
+        
+        XCTAssertFalse(a2.containsCycle(current: []))
     }
 
 }
