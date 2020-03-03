f7e6f7b70608c2da8ddc5b030edf8fa4963633b7
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-02 18:22:46 -0600

    solution to problem 282

--- swift/Problem 282/Problem 282/Solution.swift
+++ swift/Problem 282/Problem 282/Solution.swift
@@ -1,12 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 282
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-02.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Netflix.
+import Foundation
 
-Given an array of integers, determine whether it contains a Pythagorean triplet. Recall that a Pythogorean triplet `(a, b, c)` is defined by the equation `a²+ b²= c²`.
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     
     func pythogoranTriplet() -> [Int]? {
@@ -35,21 +36,3 @@ extension Array where Element == Int {
         return nil
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_282Tests: XCTestCase {
-
-    func test_pythogoran_triplet() {
-        
-        XCTAssert([24, 7, 25, 10, 2, 12].pythogoranTriplet() == [24, 7, 25])
-        XCTAssert([25, 10, 7, 2, 24, 12].pythogoranTriplet() == [7, 24, 25])
-        
-        XCTAssertNil([1].pythogoranTriplet())
-        XCTAssertNil([2, 4, 6, 8].pythogoranTriplet())
-    }
-
-}
-```
--- swift/Problem 282/Problem 282Tests/Problem_282Tests.swift
+++ swift/Problem 282/Problem 282Tests/Problem_282Tests.swift
@@ -11,24 +11,13 @@ import XCTest
 
 class Problem_282Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_pythogoran_triplet() {
+        
+        XCTAssert([24, 7, 25, 10, 2, 12].pythogoranTriplet() == [24, 7, 25])
+        XCTAssert([25, 10, 7, 2, 24, 12].pythogoranTriplet() == [7, 24, 25])
+        
+        XCTAssertNil([1].pythogoranTriplet())
+        XCTAssertNil([2, 4, 6, 8].pythogoranTriplet())
     }
 
 }
