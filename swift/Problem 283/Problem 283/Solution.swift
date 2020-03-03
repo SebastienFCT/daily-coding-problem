99a5bdf26a08f17b65a4671c4043552bd1528f79
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-03 17:53:39 -0600

    added solution of 283
    adapted from https://www.tutorialspoint.com/Ugly-Numbers

--- swift/Problem 283/Problem 283/Solution.swift
+++ swift/Problem 283/Problem 283/Solution.swift
@@ -1,16 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 283
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-03.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-A regular number in mathematics is defined as one which evenly divides some power of `60`. Equivalently, we can say that a regular number is one whose only prime divisors are `2`, `3`, and `5`.
-
-These numbers have had many applications, from helping ancient Babylonians keep time to tuning instruments according to the diatonic scale.
-
-Given an integer `N`, write a program that returns, in order, the first `N` regular numbers.
-
-## Solution
-
-```swift
 // Adaptation of https://www.tutorialspoint.com/Ugly-Numbers
 
 func regularNumbers(limit n: Int) -> [Int] {
@@ -48,17 +45,3 @@ func regularNumbers(limit n: Int) -> [Int] {
     
     return result
 }
-```
-
-## Tests
-
-```swift
-class Problem_283Tests: XCTestCase {
-
-    func test_regular_numbers() {
-        
-        XCTAssert(regularNumbers(limit: 20) == [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36])
-    }
-
-}
-```
--- swift/Problem 283/Problem 283Tests/Problem_283Tests.swift
+++ swift/Problem 283/Problem 283Tests/Problem_283Tests.swift
@@ -11,24 +11,9 @@ import XCTest
 
 class Problem_283Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_regular_numbers() {
+        
+        XCTAssert(regularNumbers(limit: 20) == [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36])
     }
 
 }
