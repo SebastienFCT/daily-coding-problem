c26aec9cad94fc50689320fd009c5625b1b381b3
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-04 17:16:52 -0600

    added problem 284

---
+++ swift/Problem 284/Problem 284/Problem_284.h
@@ -0,0 +1,19 @@
+//
+//  Problem_284.h
+//  Problem 284
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-04.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_284.
+FOUNDATION_EXPORT double Problem_284VersionNumber;
+
+//! Project version string for Problem_284.
+FOUNDATION_EXPORT const unsigned char Problem_284VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_284/PublicHeader.h>
+
+
---
+++ swift/Problem 284/README.md
@@ -0,0 +1,27 @@
+## Description
+
+This problem was asked by Yext.
+
+Two nodes in a binary tree can be called cousins if they are on the same level of the tree but have different parents. For example, in the following diagram 4 and 6 are cousins.
+
+```
+    1
+   / \
+  2   3
+ / \   \
+4   5   6
+```
+
+Given a binary tree and a particular node, find all cousins of that node.
+
+## Solution
+
+```swift
+// MARK: - TODO
+```
+
+## Tests
+
+```swift
+// MARK: - TODO
+```
