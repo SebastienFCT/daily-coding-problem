d2860d64434ef41e242cbc6428866e81ccdf84aa
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-06 16:20:03 -0600

    added problem 286

---
+++ swift/Problem 286/Problem 286/Problem_286.h
@@ -0,0 +1,19 @@
+//
+//  Problem_286.h
+//  Problem 286
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-06.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_286.
+FOUNDATION_EXPORT double Problem_286VersionNumber;
+
+//! Project version string for Problem_286.
+FOUNDATION_EXPORT const unsigned char Problem_286VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_286/PublicHeader.h>
+
+
