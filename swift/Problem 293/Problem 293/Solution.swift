9bb99eacd1edf5d58e40466fce0a654367c90dbd
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-13 21:00:00 -0500

    solution to problem 293

--- swift/Problem 293/Problem 293/Solution.swift
+++ swift/Problem 293/Problem 293/Solution.swift
@@ -1,16 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 293
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-13.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Uber.
+import Foundation
 
-You have N stones in a row, and would like to create from them a pyramid. This pyramid should be constructed such that the height of each stone increases by one until reaching the tallest stone, after which the heights decrease by one. In addition, the start and end stones of the pyramid should each be one stone high.
-
-You can change the height of any stone by paying a cost of 1 unit to lower its height by 1, as many times as necessary. Given this information, determine the lowest cost method to produce this pyramid.
-
-For example, given the stones `[1, 1, 3, 3, 2, 1]`, the optimal solution is to pay `2` to create `[0, 1, 2, 3, 2, 1]`.
-
-## Solution
-
-```swift
 struct Pyramid {
     
     var stones: [Int]
@@ -124,24 +121,3 @@ struct Pyramid {
         return sorted.first
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_293Tests: XCTestCase {
-
-    func test_example() {
-        
-        let input = Pyramid(stones: [1, 1, 3, 3, 2, 1])
-        
-        let actual = input.build()
-        let expectedCost = 2
-        let expectedResult = [0, 1, 2, 3, 2, 1]
-        
-        XCTAssert(actual?.cost == expectedCost)
-        XCTAssert(actual?.result == expectedResult)
-    }
-
-}
-```
--- swift/Problem 293/Problem 293Tests/Problem_293Tests.swift
+++ swift/Problem 293/Problem 293Tests/Problem_293Tests.swift
@@ -11,24 +11,16 @@ import XCTest
 
 class Problem_293Tests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        
+        let input = Pyramid(stones: [1, 1, 3, 3, 2, 1])
+        
+        let actual = input.build()
+        let expectedCost = 2
+        let expectedResult = [0, 1, 2, 3, 2, 1]
+        
+        XCTAssert(actual?.cost == expectedCost)
+        XCTAssert(actual?.result == expectedResult)
     }
 
 }
