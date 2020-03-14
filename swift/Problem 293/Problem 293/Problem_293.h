7e78eccf17b40b88ec53a7ebcef241f6e09f28cf
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-13 19:52:40 -0500

    added problem 293

---
+++ swift/Problem 293/Problem 293/Problem_293.h
@@ -0,0 +1,19 @@
+//
+//  Problem_293.h
+//  Problem 293
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-13.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_293.
+FOUNDATION_EXPORT double Problem_293VersionNumber;
+
+//! Project version string for Problem_293.
+FOUNDATION_EXPORT const unsigned char Problem_293VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_293/PublicHeader.h>
+
+
