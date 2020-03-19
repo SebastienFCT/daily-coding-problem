7bb4044d0105a85883f96bfd2aca894e4eb6f3b9
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-19 16:46:55 -0500

    solution to problem 299

--- swift/Problem 299/Problem 299/Solution.swift
+++ swift/Problem 299/Problem 299/Solution.swift
@@ -1,31 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 299
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-19.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Samsung.
+import Foundation
 
-A group of houses is connected to the main water plant by means of a set of pipes. A house can either be connected by a set of pipes extending directly to the plant, or indirectly by a pipe to a nearby house which is otherwise connected.
-
-For example, here is a possible configuration, where `A`, `B`, and `C` are houses, and arrows represent pipes:
-
-```
-A <--> B <--> C <--> plant
-```
-
-Each pipe has an associated cost, which the utility company would like to minimize. Given an undirected graph of pipe connections, return the lowest cost configuration of pipes such that each house has access to water.
-
-In the following setup, for example, we can remove all but the pipes from plant to `A`, plant to `B`, and `B` to `C`, for a total cost of `16`.
-
-```
-pipes = {
-    'plant': {'A': 1, 'B': 5, 'C': 20},
-    'A': {'C': 15},
-    'B': {'C': 10},
-    'C': {}
-}
-```
-
-## Solution
-
-```swift
 struct Hydro {
     
     var map: [(location: String, connections: [(location: String, cost: Int)])]
@@ -118,27 +100,3 @@ struct Hydro {
         return set
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_299Tests: XCTestCase {
-
-    func test_example() {
-        
-        let map = [
-            ("plant", [("a", 1), ("b", 5), ("c", 20)]),
-            ("a", [("c", 15)]),
-            ("b", [("c", 10)]),
-            ("c", []),
-        ]
-        
-        let input = Hydro(map: map)
-        let actual = input.optimize()
-        
-        XCTAssert(actual == 16)
-    }
-
-}
-```
