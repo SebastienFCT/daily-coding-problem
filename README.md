6705415dcaa4808ee3fdcf5e4d0b5ce79e834e40
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-20 21:52:36 -0500

    added problem 300

--- README.md
+++ README.md
@@ -5803,3 +5803,15 @@ pipes = {
 ### Solutions
 
 * [Swift](./swift/Problem&#32;299/)
+
+## Problem 300
+
+#### Description
+
+This problem was asked by Uber.
+
+On election day, a voting machine writes data in the form (`voter_id`, `candidate_id`) to a text file. Write a program that reads this file as a stream and returns the top `3` candidates at any given time. If you find a voter voting more than once, report this as fraud.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;300/)
---
+++ swift/Problem 300/Problem 300/Problem_300.h
@@ -0,0 +1,19 @@
+//
+//  Problem_300.h
+//  Problem 300
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-20.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_300.
+FOUNDATION_EXPORT double Problem_300VersionNumber;
+
+//! Project version string for Problem_300.
+FOUNDATION_EXPORT const unsigned char Problem_300VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_300/PublicHeader.h>
+
+
