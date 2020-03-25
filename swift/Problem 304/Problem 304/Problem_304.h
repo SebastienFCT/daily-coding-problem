85f4cd0d5a55a3938184773d12921a3a9f8b812c
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-24 20:29:52 -0500

    added problem 304

---
+++ swift/Problem 304/Problem 304/Problem_304.h
@@ -0,0 +1,19 @@
+//
+//  Problem_304.h
+//  Problem 304
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-24.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_304.
+FOUNDATION_EXPORT double Problem_304VersionNumber;
+
+//! Project version string for Problem_304.
+FOUNDATION_EXPORT const unsigned char Problem_304VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_304/PublicHeader.h>
+
+
