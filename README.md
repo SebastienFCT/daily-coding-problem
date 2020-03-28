a4a7c59bf9f9aaccdc54d719e599a2f37c3c4e7c
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-27 20:16:55 -0500

    added problem 306

--- README.md
+++ README.md
@@ -5896,3 +5896,17 @@ For example, suppose you are given the input `3 -> 4 -> -7 -> 5 -> -6 -> 6`. In 
 ### Solutions
 
 * [Swift](./swift/Problem&#32;305/)
+
+## Problem 306
+
+#### Description
+
+This problem was asked by Palantir.
+
+You are given a list of N numbers, in which each number is located at most k places away from its sorted position. For example, if k = 1, a given element at index 4 might end up at indices 3, 4, or 5.
+
+Come up with an algorithm that sorts this list in O(N log k) time.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;306/)
---
+++ swift/Problem 306/Problem 306/Problem_306.h
@@ -0,0 +1,19 @@
+//
+//  Problem_306.h
+//  Problem 306
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-27.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_306.
+FOUNDATION_EXPORT double Problem_306VersionNumber;
+
+//! Project version string for Problem_306.
+FOUNDATION_EXPORT const unsigned char Problem_306VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_306/PublicHeader.h>
+
+
