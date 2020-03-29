c16c36ec170bb46229a20862571b2a54d6e83445
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-29 12:12:52 -0500

    solution to problem 309

--- swift/Problem 309/Problem 309/Solution.swift
+++ swift/Problem 309/Problem 309/Solution.swift
@@ -1,16 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 309
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-29.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Walmart Labs.
+import Foundation
 
-There are `M` people sitting in a row of `N` seats, where `M < N`. Your task is to redistribute people such that there are no gaps between any of them, while keeping overall movement to a minimum.
-
-For example, suppose you are faced with an input of `[0, 1, 1, 0, 1, 0, 0, 0, 1]`, where `0` represents an empty seat and `1` represents a person. In this case, one solution would be to place the person on the right in the fourth seat. We can consider the cost of a solution to be the sum of the absolute distance each person must move, so that the cost here would be five.
-
-Given an input such as the one above, return the lowest possible cost of moving people to remove all gaps.
-
-## Solution
-
-```swift
 extension Array where Element == Int {
     
     // Calling it defragmentation as it reminds me these degragmentation diagram on Windows 98: https://www.youtube.com/watch?v=dc_SDyLYq3U
@@ -103,22 +100,3 @@ extension Array where Element == Int {
         return true
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_309Tests: XCTestCase {
-
-    func test_example() {
-        
-        let input = [0, 1, 1, 0, 1, 0, 0, 0, 1]
-        
-        let actual = input.minCostForDefragmentation()
-        let expected = 5
-        
-        XCTAssert(actual == expected)
-    }
-
-}
-```
