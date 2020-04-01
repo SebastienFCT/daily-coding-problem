835d02728761f2dad00d090996ff2d32288369b5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-03-31 22:36:52 -0500

    added problem 311

--- README.md
+++ README.md
@@ -5971,3 +5971,17 @@ Write an algorithm that finds the total number of set bits in all integers betwe
 ### Solutions
 
 * [Swift](./swift/Problem&#32;310/)
+
+## Problem 311
+
+#### Description
+
+This problem was asked by Sumo Logic.
+
+Given an unsorted array, in which all elements are distinct, find a "peak" element in O(log N) time.
+
+An element is considered a peak if it is greater than both its left and right neighbors. It is guaranteed that the first and last elements are lower than all others.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;311/)
---
+++ swift/Problem 311/Problem 311/Problem_311.h
@@ -0,0 +1,19 @@
+//
+//  Problem_311.h
+//  Problem 311
+//
+//  Created by sebastien FOCK CHOW THO on 2020-03-31.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_311.
+FOUNDATION_EXPORT double Problem_311VersionNumber;
+
+//! Project version string for Problem_311.
+FOUNDATION_EXPORT const unsigned char Problem_311VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_311/PublicHeader.h>
+
+
