dafecef6a721fa68634beae5a11ec680544dcccc
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-03 19:06:43 -0500

    added problem 314

---
+++ swift/Problem 314/Problem 314/Problem_314.h
@@ -0,0 +1,19 @@
+//
+//  Problem_314.h
+//  Problem 314
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-03.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_314.
+FOUNDATION_EXPORT double Problem_314VersionNumber;
+
+//! Project version string for Problem_314.
+FOUNDATION_EXPORT const unsigned char Problem_314VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_314/PublicHeader.h>
+
+
