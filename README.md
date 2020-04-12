ec3ef98a994eda70f96be900ebacf8dbae59df1a
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-11 22:15:56 -0500

    added problem 322

--- README.md
+++ README.md
@@ -6153,3 +6153,19 @@ For example, given `100`, you can reach `1` in five steps with the following rou
 ### Solutions
 
 * [Swift](./swift/Problem&#32;321/)
+
+## Problem 322
+
+#### Description
+
+This problem was asked by Flipkart.
+
+Starting from 0 on a number line, you would like to make a series of jumps that lead to the integer N.
+
+On the ith jump, you may move exactly i places to the left or right.
+
+Find a path with the fewest number of jumps required to get from 0 to N.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;322/)
---
+++ swift/Problem 322/Problem 322/Problem_322.h
@@ -0,0 +1,19 @@
+//
+//  Problem_322.h
+//  Problem 322
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-11.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_322.
+FOUNDATION_EXPORT double Problem_322VersionNumber;
+
+//! Project version string for Problem_322.
+FOUNDATION_EXPORT const unsigned char Problem_322VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_322/PublicHeader.h>
+
+
