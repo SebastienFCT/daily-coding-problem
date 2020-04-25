787637c227b819411d302cf8d70b49b00b4a27d5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-25 10:12:41 -0500

    added problem 335

---
+++ swift/Problem 335/Problem 335/Problem_335.h
@@ -0,0 +1,19 @@
+//
+//  Problem_335.h
+//  Problem 335
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-25.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_335.
+FOUNDATION_EXPORT double Problem_335VersionNumber;
+
+//! Project version string for Problem_335.
+FOUNDATION_EXPORT const unsigned char Problem_335VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_335/PublicHeader.h>
+
+
