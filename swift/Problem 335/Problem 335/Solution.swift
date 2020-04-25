90e04a925834186136dff240762a8c18037b3a6d
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-25 10:56:52 -0500

    solution to problem 335

--- swift/Problem 335/Problem 335/Solution.swift
+++ swift/Problem 335/Problem 335/Solution.swift
@@ -1,20 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 335
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-25.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-PageRank is an algorithm used by Google to rank the importance of different websites. While there have been changes over the years, the central idea is to assign each site a score based on the importance of other pages that link to that page.
-
-More mathematically, suppose there are `N` sites, and each site `i` has a certain count `Ci` of outgoing links. Then the score for a particular site `Sj` is defined as :
-
-`score(Sj) = (1 - d) / N + d * (score(Sx) / Cx+ score(Sy) / Cy+ ... + score(Sz) / Cz))`
-
-Here, `Sx`, `Sy`, ..., `Sz` denote the scores of all the other sites that have outgoing links to `Sj`, and `d` is a `damping factor`, usually set to around 0.85, used to model the probability that a user will stop searching.
-
-Given a directed graph of links between various websites, write a program that calculates each site's page rank.
-
-## Solution
-
-```swift
 class Node {
     var value: String
     var pointers: [Node]
@@ -80,29 +73,3 @@ struct Web {
         return result
     }
 }
-
-```
-
-## Tests
-
-```swift
-class Problem_335Tests: XCTestCase {
-
-    func test_page_ranking() {
-        
-        let web1 = Node(value: "web1")
-        let web2 = Node(value: "web2")
-        let web3 = Node(value: "web3")
-        let web4 = Node(value: "web4")
-        
-        web1.pointers = [web2, web3]
-        web3.pointers = [web2]
-        web2.pointers = [web4]
-        
-        let input = Web(pages: [web1, web2, web3], dampingFactor: 0.85)
-        
-        print(input.ranking())
-    }
-
-}
-```
