37327c1eab481b099044a627449ca013749d4d6f
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-28 16:27:29 -0500

    added problem 339

--- README.md
+++ README.md
@@ -6466,3 +6466,15 @@ Given an integer n, find the next biggest integer with the same number of 1-bits
 ### Solutions
 
 * [Swift](./swift/Problem&#32;338/)
+
+## Problem 339
+
+#### Description
+
+This problem was asked by Microsoft.
+
+Given an array of numbers and a number k, determine if there are three entries in the array which add up to the specified number `k`. For example, given `[20, 303, 3, 4, 25]` and `k = 49`, return true as `20 + 4 + 25 = 49`.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;339/)
---
+++ swift/Problem 339/Problem 339/Problem_339.h
@@ -0,0 +1,19 @@
+//
+//  Problem_339.h
+//  Problem 339
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-28.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_339.
+FOUNDATION_EXPORT double Problem_339VersionNumber;
+
+//! Project version string for Problem_339.
+FOUNDATION_EXPORT const unsigned char Problem_339VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_339/PublicHeader.h>
+
+
