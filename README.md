871bb75c033f378446c89fb799e36da7618850bc
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-04-29 16:41:21 -0500

    added problem 340

--- README.md
+++ README.md
@@ -6478,3 +6478,15 @@ Given an array of numbers and a number k, determine if there are three entries i
 ### Solutions
 
 * [Swift](./swift/Problem&#32;339/)
+
+## Problem 340
+
+#### Description
+
+This problem was asked by Google.
+
+Given a set of points (x, y) on a 2D cartesian plane, find the two closest points. For example, given the points `[(1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)]`, return `[(-1, -1), (1, 1)]`.
+
+### Solutions
+
+* [Swift](./swift/Problem&#32;340/)
---
+++ swift/Problem 340/Problem 340/Problem_340.h
@@ -0,0 +1,19 @@
+//
+//  Problem_340.h
+//  Problem 340
+//
+//  Created by sebastien FOCK CHOW THO on 2020-04-29.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_340.
+FOUNDATION_EXPORT double Problem_340VersionNumber;
+
+//! Project version string for Problem_340.
+FOUNDATION_EXPORT const unsigned char Problem_340VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_340/PublicHeader.h>
+
+
