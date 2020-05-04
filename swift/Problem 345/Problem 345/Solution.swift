661a5b91df5b56b69650a59222d8d57b4ac50bd5
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-04 17:31:33 -0500

    partial solution. Will finish later

--- swift/Problem 345/Problem 345/Solution.swift
+++ swift/Problem 345/Problem 345/Solution.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+
+typealias Synonym = (String, String)
+
+func areTheSame(left: String, right: String, synonyms: [Synonym], connectSynonyms: Bool) -> Bool {
+    
+    let leftWords = left.trimmingCharacters(in: CharacterSet.punctuationCharacters).split(separator: " ")
+    let rightWords = right.trimmingCharacters(in: CharacterSet.punctuationCharacters).split(separator: " ")
+    
+    guard leftWords.count == rightWords.count else {
+        return false
+    }
+    
+    if !connectSynonyms {
+        for i in 0..<leftWords.count {
+            
+            if leftWords[i] == rightWords[i] { continue }
+            
+            guard synonyms.contains(where: { (element) -> Bool in
+                (element.0 == leftWords[i] && element.1 == rightWords[i]) || (element.1 == leftWords[i] && element.0 == rightWords[i])
+            }) else {
+                return false
+            }
+        }
+    } else {
+        
+    }
+    
+    return true
+}
+
+func areSynonyms(left: String, right: String, withConnectedSynonyms synonyms: [Synonym], seen: [String]) -> Bool {
+    
+    var candidates: [String] = []
+    
+    if synonyms.contains(where: { (element) -> Bool in
+        (element.0 == left && element.1 == right) || (element.1 == left && element.0 == right)
+    }) {
+        return true
+    }
+    
+    var newSeens = seen
+    
+    for synonym in synonyms {
+        if (synonym.0 == left && synonym.1 == right) || (synonym.1 == left && synonym.0 == right) {
+            return true
+        }
+        
+        if synonym.0 == left && !seen.contains(where: { (item) -> Bool in
+            item == synonym.1
+        }) {
+            newSeens.append(synonym.1)
+            candidates.append(synonym.1)
+        }
+        
+        if synonym.1 == left && !seen.contains(where: { (item) -> Bool in
+            item == synonym.0
+        }) {
+            newSeens.append(synonym.0)
+            candidates.append(synonym.0)
+        }
+    }
+    
+    for candidate in candidates {
+        
+        if areSynonyms(left: candidate, right: right, withConnectedSynonyms: synonyms, seen: newSeens) {
+            return true
+        }
+    }
+    
+    return false
+}
--- swift/Problem 345/Problem 345Tests/Problem_345Tests.swift
+++ swift/Problem 345/Problem 345Tests/Problem_345Tests.swift
@@ -11,24 +11,17 @@ import XCTest
 
 class Problem_345Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        
+        let synonyms: [Synonym] = [
+            ("eat", "consume"),
+            ("consume", "absorb")
+        ]
+        
+        XCTAssertTrue(areTheSame(left: "He wants to eat food.", right: "He wants to consume food.", synonyms: synonyms, connectSynonyms: false))
+        
+        XCTAssertFalse(areTheSame(left: "He wants to eat food.", right: "He wants to absorb food.", synonyms: synonyms, connectSynonyms: false))
+        XCTAssertFalse(areTheSame(left: "He wants to eat food.", right: "He wants to absorb food.", synonyms: synonyms, connectSynonyms: true))
     }
 
 }
