b6f8db7ab08ed6a17692cab1b65f1430553c2d28
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-06 09:16:18 -0500

    solution to problem 347

--- swift/Problem 347/Problem 347/Solution.swift
+++ swift/Problem 347/Problem 347/Solution.swift
@@ -1,16 +1,27 @@
-## Description
+//
+//  Solution.swift
+//  Problem 347
+//
+//  Created by sebastien FOCK CHOW THO on 2020-05-06.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Yahoo.
+import Foundation
 
-You are given a string of length `N` and a parameter `k`. The string can be manipulated by taking one of the first `k` letters and moving it to the end.
+//aedcb, k=2
+//adcbe
+//dcbea
+//cbead
+//ceadb
+//eadbc
+//adbce
+//abced
+//bceda
+//cedab
+//edabc
+//eabcd
+//abcde
 
-Write a program to determine the lexicographically smallest string that can be created after an unlimited number of moves.
-
-For example, suppose we are given the string daily and `k = 1`. The best we can create in this case is ailyd.
-
-## Solution
-
-```swift
 extension String {
     
     func lexicoMin(movingAnyOfTheFirsts: Int) -> String {
@@ -64,21 +75,3 @@ extension StringProtocol {
     subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
     subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
 }
-```
-
-## Tests
-
-```swift
-class Problem_347Tests: XCTestCase {
-
-    func test_example() {
-        
-        let input = "daily"
-        let actual = input.lexicoMin(movingAnyOfTheFirsts: 1)
-        let expected = "ailyd"
-        
-        XCTAssert(actual == expected)
-    }
-
-}
-```
