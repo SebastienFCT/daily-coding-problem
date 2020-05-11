064c7f4c5369f53171f8bb7abb77572b6c2e91ce
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-11 18:09:07 -0500

    added problem 352

---
+++ swift/Problem 352/Problem 352/Problem_352.h
@@ -0,0 +1,19 @@
+//
+//  Problem_352.h
+//  Problem 352
+//
+//  Created by sebastien FOCK CHOW THO on 2020-05-11.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_352.
+FOUNDATION_EXPORT double Problem_352VersionNumber;
+
+//! Project version string for Problem_352.
+FOUNDATION_EXPORT const unsigned char Problem_352VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_352/PublicHeader.h>
+
+
