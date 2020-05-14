407c4d514ec1b465a501e17e4110d95e7b5839ca
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-14 17:31:02 -0500

    added problem 355

---
+++ swift/Problem 355/Problem 355/Problem_355.h
@@ -0,0 +1,19 @@
+//
+//  Problem_355.h
+//  Problem 355
+//
+//  Created by sebastien FOCK CHOW THO on 2020-05-14.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Problem_355.
+FOUNDATION_EXPORT double Problem_355VersionNumber;
+
+//! Project version string for Problem_355.
+FOUNDATION_EXPORT const unsigned char Problem_355VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Problem_355/PublicHeader.h>
+
+
