51f9026ccdfee6cfa2105b210f1e0e441cbbb69a
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-13 19:27:15 -0500

    abstract solution added for 354

--- swift/Problem 354/Problem 354/Solution.swift
+++ swift/Problem 354/Problem 354/Solution.swift
@@ -1,21 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 354
+//
+//  Created by sebastien FOCK CHOW THO on 2020-05-13.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Google.
+import Foundation
 
-Design a system to crawl and copy all of Wikipedia using a distributed network of machines.
-
-More specifically, suppose your server has access to a set of client machines. Your client machines can execute code you have written to access Wikipedia pages, download and parse their data, and write the results to a database.
-
-Some questions you may want to consider as part of your solution are:
-
-- How will you reach as many pages as possible?
-- How can you keep track of pages that have already been visited?
-- How will you deal with your client machines being blacklisted?
-- How can you update your database when Wikipedia pages are added or updated?
-
-## Solution
-
-```swift
 struct Client {
     
     func process(url: URL) -> String? {
@@ -70,10 +62,3 @@ struct Master {
         return result
     }
 }
-```
-
-## Tests
-
-```swift
-// MARK: - TODO
-```
