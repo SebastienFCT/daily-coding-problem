dfc4c6cdc061417887c59f6205e74875cc453079
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2020-05-14 18:05:15 -0500

    solution to problem 355

--- swift/Problem 355/Problem 355/Solution.swift
+++ swift/Problem 355/Problem 355/Solution.swift
@@ -1,19 +1,13 @@
-## Description
+//
+//  Solution.swift
+//  Problem 355
+//
+//  Created by sebastien FOCK CHOW THO on 2020-05-14.
+//  Copyright © 2020 sebastien FOCK CHOW THO. All rights reserved.
+//
 
-This problem was asked by Airbnb.
+import Foundation
 
-You are given an array `X` of floating-point numbers `x1`, `x2`, ... `xn`. These can be rounded up or down to create a corresponding array `Y` of integers `y1`, `y2`, ... `yn`.
-
-Write an algorithm that finds an appropriate `Y` array with the following properties:
-
-- The rounded sums of both arrays should be equal.
-- The absolute pairwise difference between elements is minimized. In other words, `|x1- y1| + |x2- y2| + ... + |xn- yn|` should be as small as possible.
-
-For example, suppose your input is `[1.3, 2.3, 4.4]`. In this case you cannot do better than `[1, 2, 5]`, which has an absolute difference of `|1.3 - 1| + |2.3 - 2| + |4.4 - 5| = 1`.
-
-## Solution
-
-```swift
 extension Array where Element == Double {
     
     func roundedArrayWithSmallerPairwiseDiff() -> [Int] {
@@ -72,21 +66,3 @@ extension Array where Element == Double {
         return candidates
     }
 }
-```
-
-## Tests
-
-```swift
-class Problem_355Tests: XCTestCase {
-
-    func test_example() {
-        let input: [Double] = [1.3, 2.3, 4.4]
-        
-        let actual = input.roundedArrayWithSmallerPairwiseDiff()
-        let expected = [1, 2, 5]
-        
-        XCTAssert(actual == expected)
-    }
-
-}
-```
--- swift/Problem 355/Problem 355Tests/Problem_355Tests.swift
+++ swift/Problem 355/Problem 355Tests/Problem_355Tests.swift
@@ -11,24 +11,13 @@ import XCTest
 
 class Problem_355Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_example() {
+        let input: [Double] = [1.3, 2.3, 4.4]
+        
+        let actual = input.roundedArrayWithSmallerPairwiseDiff()
+        let expected = [1, 2, 5]
+        
+        XCTAssert(actual == expected)
     }
 
 }
